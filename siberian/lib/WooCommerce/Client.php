5fb3917df1d42b6c5635eee0dfc8c5114f8262bb
Alex-Yves CREPIN <a.crepin@xtraball.com>
2017-11-14 14:36:06 +0100

    chore: reworked/split all .gitignore files, easier to read and maintain.

---
+++ siberian/lib/WooCommerce/Client.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ * WooCommerce REST API Client
+ *
+ * @category Client
+ * @package  Automattic/WooCommerce
+ */
+
+namespace WooCommerce;
+
+use WooCommerce\HttpClient\HttpClient;
+
+/**
+ * REST API Client class.
+ *
+ * @package Automattic/WooCommerce
+ */
+class Client
+{
+
+    /**
+     * WooCommerce REST API Client version.
+     */
+    const VERSION = '1.1.2';
+
+    /**
+     * HttpClient instance.
+     *
+     * @var HttpClient
+     */
+    public $http;
+
+    /**
+     * Initialize client.
+     *
+     * @param string $url            Store URL.
+     * @param string $consumerKey    Consumer key.
+     * @param string $consumerSecret Consumer secret.
+     * @param array  $options        Options (version, timeout, verify_ssl).
+     */
+    public function __construct($url, $consumerKey, $consumerSecret, $options = [])
+    {
+        $this->http = new HttpClient($url, $consumerKey, $consumerSecret, $options);
+    }
+
+    /**
+     * POST method.
+     *
+     * @param string $endpoint API endpoint.
+     * @param array  $data     Request data.
+     *
+     * @return array
+     */
+    public function post($endpoint, $data)
+    {
+        return $this->http->request($endpoint, 'POST', $data);
+    }
+
+    /**
+     * PUT method.
+     *
+     * @param string $endpoint API endpoint.
+     * @param array  $data     Request data.
+     *
+     * @return array
+     */
+    public function put($endpoint, $data)
+    {
+        return $this->http->request($endpoint, 'PUT', $data);
+    }
+
+    /**
+     * GET method.
+     *
+     * @param string $endpoint   API endpoint.
+     * @param array  $parameters Request parameters.
+     *
+     * @return array
+     */
+    public function get($endpoint, $parameters = [])
+    {
+        return $this->http->request($endpoint, 'GET', [], $parameters);
+    }
+
+    /**
+     * DELETE method.
+     *
+     * @param string $endpoint   API endpoint.
+     * @param array  $parameters Request parameters.
+     *
+     * @return array
+     */
+    public function delete($endpoint, $parameters = [])
+    {
+        return $this->http->request($endpoint, 'DELETE', [], $parameters);
+    }
+
+    /**
+     * OPTIONS method.
+     *
+     * @param string $endpoint API endpoint.
+     *
+     * @return array
+     */
+    public function options($endpoint)
+    {
+        return $this->http->request($endpoint, 'OPTIONS', [], []);
+    }
+}
---
+++ siberian/lib/WooCommerce/HttpClient/Options.php
@@ -0,0 +1,112 @@
+<?php
+/**
+ * WooCommerce REST API HTTP Client Options
+ *
+ * @category HttpClient
+ * @package  Automattic/WooCommerce
+ */
+
+namespace WooCommerce\HttpClient;
+
+/**
+ * REST API HTTP Client Options class.
+ *
+ * @package Automattic/WooCommerce
+ */
+class Options
+{
+
+    /**
+     * Default WooCommerce REST API version.
+     */
+    const VERSION = 'v3';
+
+    /**
+     * Default request timeout.
+     */
+    const TIMEOUT = 15;
+
+    /**
+     * Default WP API prefix.
+     * Including leading and trailing slashes.
+     */
+    const WP_API_PREFIX = '/wp-json/';
+
+    /**
+     * Options.
+     *
+     * @var array
+     */
+    private $options;
+
+    /**
+     * Initialize HTTP client options.
+     *
+     * @param array $options Client options.
+     */
+    public function __construct($options)
+    {
+        $this->options = $options;
+    }
+
+    /**
+     * Get API version.
+     *
+     * @return string
+     */
+    public function getVersion()
+    {
+        return isset($this->options['version']) ? $this->options['version'] : self::VERSION;
+    }
+
+    /**
+     * Check if need to verify SSL.
+     *
+     * @return bool
+     */
+    public function verifySsl()
+    {
+        return isset($this->options['verify_ssl']) ? (bool) $this->options['verify_ssl'] : true;
+    }
+
+    /**
+     * Get timeout.
+     *
+     * @return int
+     */
+    public function getTimeout()
+    {
+        return isset($this->options['timeout']) ? (int) $this->options['timeout'] : self::TIMEOUT;
+    }
+
+    /**
+     * Basic Authentication as query string.
+     * Some old servers are not able to use CURLOPT_USERPWD.
+     *
+     * @return bool
+     */
+    public function isQueryStringAuth()
+    {
+        return isset($this->options['query_string_auth']) ? (bool) $this->options['query_string_auth'] : false;
+    }
+
+    /**
+     * Check if is WP REST API.
+     *
+     * @return bool
+     */
+    public function isWPAPI()
+    {
+        return isset($this->options['wp_api']) ? (bool) $this->options['wp_api'] : false;
+    }
+
+    /**
+     * Custom API Prefix for WP API.
+     *
+     * @return string
+     */
+    public function apiPrefix()
+    {
+        return isset($this->options['wp_api_prefix']) ? $this->options['wp_api_prefix'] : self::WP_API_PREFIX;
+    }
+}
