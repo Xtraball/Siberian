413a8fc660658ac59c4c120f7995b85b0e68f142
Alex-Yves CREPIN <a.crepin@xtraball.com>
2017-11-14 19:12:38 +0100

    fix: install build script, remove ios-deploy un-necessary deps, delete deprecated github build script.

--- ci/build-installs.sh
+++ ci/build-installs.sh
@@ -4,7 +4,6 @@
 RELEASE=$(node -e "console.log(require('./package.json').version);")
 NATIVE_VERSION=$(node -e "console.log(require('./package.json').nativeVersion);")
 API_VERSION=$(node -e "console.log(require('./package.json').apiVersion);")
-HASH_TO=$(git rev-parse "v"$RELEASE^0)
 
 # Builds hashes
 HSAE="xoNJw0qczqXUsRQqW0ubNGUXHtmxNANp"
@@ -25,47 +24,15 @@ PPE=$BUILDS"/install-pe"
 TAR_EXCLUDE="--exclude='*.DS_Store*' --exclude='*.idea*' --exclude='*.gitignore*' --exclude='*.localized*'"
 
 # Clean up previous builds
+mkdir -p $BUILDS
 cd $BUILDS
 rm -rf "*install*"
 cd -
 rm -f $ROOT"/change.txt"
 rm -f $ROOT"/delete.txt"
-mkdir -p $BUILDS
 mkdir -p $PSAE
 mkdir -p $PMAE
 mkdir -p $PPE
-
-# Clean up just in case
-rm -Rf $SIBERIAN/system.php
-rm -Rf $SIBERIAN/var/schema
-rm -Rf $SIBERIAN/var/tmp/*
-rm -Rf $SIBERIAN/var/log/*
-rm -Rf $SIBERIAN/var/cache/*
-rm -Rf $SIBERIAN/var/apps/angular/iphone/*
-rm -Rf $SIBERIAN/var/apps/angular/android/*
-rm -Rf $SIBERIAN/var/apps/ionic/tools/android-sdk
-rm -Rf $SIBERIAN/var/apps/ionic/tools/gradle
-
-rm -Rf $SIBERIAN/images/templates/accessories
-rm -Rf $SIBERIAN/images/templates/b_b
-rm -Rf $SIBERIAN/images/templates/band
-rm -Rf $SIBERIAN/images/templates/beauty_center
-rm -Rf $SIBERIAN/images/templates/big_company
-rm -Rf $SIBERIAN/images/templates/club
-rm -Rf $SIBERIAN/images/templates/corporate
-rm -Rf $SIBERIAN/images/templates/fitness
-rm -Rf $SIBERIAN/images/templates/florist
-rm -Rf $SIBERIAN/images/templates/football_team
-rm -Rf $SIBERIAN/images/templates/grande_palace
-rm -Rf $SIBERIAN/images/templates/jewellery
-rm -Rf $SIBERIAN/images/templates/lawyer
-rm -Rf $SIBERIAN/images/templates/nyc
-rm -Rf $SIBERIAN/images/templates/paris
-rm -Rf $SIBERIAN/images/templates/publisher
-rm -Rf $SIBERIAN/images/templates/real_estate_one
-rm -Rf $SIBERIAN/images/templates/rush_cafe
-rm -Rf $SIBERIAN/images/templates/surgery
-
 
 # Building SAE - Install
 cp -rp $SIBERIAN/* $PSAE/
