d4db92a84e038064c8a22deead4fe6f778419a84
Alex-Yves CREPIN <a.crepin@xtraball.com>
2017-11-15 11:58:28 +0100

    feat: reduce installer, update & hotfix script builders to SAE scope.

--- ci/pack-update.sh
+++ ci/pack-update.sh
@@ -8,7 +8,6 @@ API_VERSION=$(node -e "console.log(require('./package.json').apiVersion);")
 HASH_FROM=$(git rev-parse "v"$REQUIRED_VERSION^0)
 HASH_TO=$(git rev-parse "v"$RELEASE^0)
 
-
 # Paths
 ROOT=$PWD
 REPO=$ROOT
@@ -26,9 +25,7 @@ cd -
 rm -f $ROOT"/change.txt"
 rm -f $ROOT"/delete.txt"
 mkdir -p $BUILDS
-mkdir -p $BUILDS/update-sae
-mkdir -p $BUILDS/update-mae
-mkdir -p $BUILDS/update-pe
+mkdir -p $BUILDS/update-package
 
 # Files to include (or not)
 git diff --name-status --relative=siberian/ $HASH_FROM $HASH_TO |grep "^\(M\|A\|R\|T\)" |cut -f 2 > $ROOT"/change.txt"
@@ -57,28 +54,23 @@ TODELETE=$(echo $TODELETE | sed -e 's/\//\\\//g' -e 's/\(,$\)//g')
 while read FILE; do
   DIRNAME=$(dirname $SIBERIAN"/"$FILE)
   BASEDIR=${DIRNAME/$SIBERIAN/}
-  if [ ! -d $BUILDS"/update-sae/"$BASEDIR ];then
-    mkdir -p $BUILDS"/update-sae/"$BASEDIR
+  if [ ! -d $BUILDS"/update-package/"$BASEDIR ];then
+    mkdir -p $BUILDS"/update-package/"$BASEDIR
   fi
-  cp $SIBERIAN"/"$FILE $BUILDS"/update-sae/"$BASEDIR
+  cp $SIBERIAN"/"$FILE $BUILDS"/update-package/"$BASEDIR
 done < $ROOT"/change.txt"
 
-# Force include files
-#mkdir -p $BUILDS"/update-sae/var/apps/ionic/"
-#cp -Rf $SIBERIAN"/var/apps/ionic/previewer" $BUILDS"/update-sae/var/apps/ionic/"
+php -f $REPO"/ci/scripts/manifest.php" sae $SIBERIAN"/" $BUILDS"/update-package/"
 
-rm -rf $BUILDS/update-sae/app/pe
-rm -rf $BUILDS/update-sae/app/mae
-php -f $REPO"/ci/scripts/manifest.php" sae $SIBERIAN"/" $BUILDS"/update-sae/"
 # Clean up new logo
-rm -rf $BUILDS/update-sae/app/sae/design/desktop/flat/images/header/logo.png
-mkdir -p $BUILDS/update-sae/lib/Siberian/
+rm -rf $BUILDS/update-package/app/sae/design/desktop/flat/images/header/logo.png
+mkdir -p $BUILDS/update-package/lib/Siberian/
 cat $REPO/bin/templates/Version.php | \
     sed -e s/%VERSION%/$RELEASE/g \
     -e s/%NATIVE_VERSION%/$NATIVE_VERSION/g \
     -e s/%API_VERSION%/$API_VERSION/g \
     -e "s/%NAME%/Single App Edition/g" \
-    -e "s/%TYPE%/SAE/g" > $BUILDS/update-sae/lib/Siberian/Version.php
+    -e "s/%TYPE%/SAE/g" > $BUILDS/update-package/lib/Siberian/Version.php
 cat $TEMPLATES/package-update.json | \
     sed -e s/%VERSION%/$RELEASE/g \
     -e s/%NATIVE_VERSION%/$NATIVE_VERSION/g \
@@ -87,92 +79,13 @@ cat $TEMPLATES/package-update.json | \
     -e "s/%TYPE%/SAE/g" \
     -e "s/%TYPE_LOWER%/sae/g" \
     -e s/%TO_DELETE%/$TODELETE/g \
-    -e s/%REQUIRED_VERSION%/$REQUIRED_VERSION/g > $BUILDS/update-sae/package.json
+    -e s/%REQUIRED_VERSION%/$REQUIRED_VERSION/g > $BUILDS/update-package/package.json
 # Dive into folder then zip
-cd $BUILDS/update-sae/
-zip -r -9 $ZIP_EXCLUDE ../siberian_sae.update.$RELEASE.zip ./
+cd $BUILDS/update-package/
+zip -r -9 $ZIP_EXCLUDE ../sae.update.$RELEASE.zip ./
 
-
-# Building MAE - Update
-while read FILE; do
-  DIRNAME=$(dirname $SIBERIAN"/"$FILE)
-  BASEDIR=${DIRNAME/$SIBERIAN/}
-  if [ ! -d $BUILDS"/update-mae/"$BASEDIR ];then
-    mkdir -p $BUILDS"/update-mae/"$BASEDIR
-  fi
-  cp $SIBERIAN"/"$FILE $BUILDS"/update-mae/"$BASEDIR
-done < $ROOT"/change.txt"
-
-# Force include files
-#mkdir -p $BUILDS"/update-mae/var/apps/ionic/"
-#cp -Rf $SIBERIAN"/var/apps/ionic/previewer" $BUILDS"/update-mae/var/apps/ionic/"
-
-
-rm -rf $BUILDS/update-mae/app/pe
-php -f $REPO"/ci/scripts/manifest.php" mae $SIBERIAN"/" $BUILDS"/update-mae/"
-# Clean up new logo
-rm -rf $BUILDS/update-sae/app/sae/design/desktop/flat/images/header/logo.png
-mkdir -p $BUILDS/update-mae/lib/Siberian/
-cat $REPO/bin/templates/Version.php | \
-    sed -e s/%VERSION%/$RELEASE/g \
-    -e s/%NATIVE_VERSION%/$NATIVE_VERSION/g \
-    -e s/%API_VERSION%/$API_VERSION/g \
-    -e "s/%NAME%/Multi-Apps Edition/g" \
-    -e "s/%TYPE%/MAE/g" > $BUILDS/update-mae/lib/Siberian/Version.php
-cat $TEMPLATES/package-update.json | \
-    sed -e s/%VERSION%/$RELEASE/g \
-    -e s/%NATIVE_VERSION%/$NATIVE_VERSION/g \
-    -e s/%API_VERSION%/$API_VERSION/g \
-    -e "s/%NAME%/Multi-Apps Edition/g" \
-    -e "s/%TYPE%/MAE/g" \
-    -e "s/%TYPE_LOWER%/mae/g" \
-    -e s/%TO_DELETE%/$TODELETE/g \
-    -e s/%REQUIRED_VERSION%/$REQUIRED_VERSION/g > $BUILDS/update-mae/package.json
-# Dive into folder then zip
-cd $BUILDS/update-mae/
-zip -r -9 $ZIP_EXCLUDE ../siberian_mae.update.$RELEASE.zip ./
-
-
-# Building PE - Update
-while read FILE; do
-  DIRNAME=$(dirname $SIBERIAN"/"$FILE)
-  BASEDIR=${DIRNAME/$SIBERIAN/}
-  if [ ! -d $BUILDS"/update-pe/"$BASEDIR ];then
-    mkdir -p $BUILDS"/update-pe/"$BASEDIR
-  fi
-  cp $SIBERIAN"/"$FILE $BUILDS"/update-pe/"$BASEDIR
-done < $ROOT"/change.txt"
-
-# Force include files
-#mkdir -p $BUILDS"/update-pe/var/apps/ionic/"
-#cp -Rf $SIBERIAN"/var/apps/ionic/previewer" $BUILDS"/update-pe/var/apps/ionic/"
-
-php -f $REPO"/ci/scripts/manifest.php" pe $SIBERIAN"/" $BUILDS"/update-pe/"
-# Clean up new logo
-rm -rf $BUILDS/update-sae/app/sae/design/desktop/flat/images/header/logo.png
-mkdir -p $BUILDS/update-pe/lib/Siberian/
-cat $REPO/bin/templates/Version.php | \
-    sed -e s/%VERSION%/$RELEASE/g \
-    -e s/%NATIVE_VERSION%/$NATIVE_VERSION/g \
-    -e s/%API_VERSION%/$API_VERSION/g \
-    -e "s/%NAME%/Platform Edition/g" \
-    -e "s/%TYPE%/PE/g" > $BUILDS/update-pe/lib/Siberian/Version.php
-cat $TEMPLATES/package-update.json | \
-    sed -e s/%VERSION%/$RELEASE/g \
-    -e s/%NATIVE_VERSION%/$NATIVE_VERSION/g \
-    -e s/%API_VERSION%/$API_VERSION/g \
-    -e "s/%NAME%/Platform Edition/g" \
-    -e "s/%TYPE%/PE/g" \
-    -e "s/%TYPE_LOWER%/pe/g" \
-    -e s/%TO_DELETE%/$TODELETE/g \
-    -e s/%REQUIRED_VERSION%/$REQUIRED_VERSION/g > $BUILDS/update-pe/package.json
-# Dive into folder then zip
-cd $BUILDS/update-pe/
-zip -r -9 $ZIP_EXCLUDE ../siberian_pe.update.$RELEASE.zip ./
 
 # Clean-up
-rm -rf $BUILDS/update-sae
-rm -rf $BUILDS/update-mae
-rm -rf $BUILDS/update-pe
+rm -rf $BUILDS/update-package
 rm -f $ROOT"/delete.txt"
 rm -f $ROOT"/change.txt"
