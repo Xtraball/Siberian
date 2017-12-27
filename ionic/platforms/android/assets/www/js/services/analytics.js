7912f8d88f161728850211c4928e801532ef8a7c
Alex-Yves CREPIN <a.crepin@xtraball.com>
2017-12-27 11:27:05 +0100

    chore: prepped platforms

--- ionic/platforms/android/assets/www/js/services/analytics.js
+++ ionic/platforms/android/assets/www/js/services/analytics.js
@@ -1,197 +1,121 @@
-/* global
-    App, device, cordova
+/**
+ * Analytics request handler!
  */
-angular.module('starter').service('Analytics', function ($cordovaGeolocation, $pwaRequest, $q, $log, $rootScope,
-                                                         Application, Url) {
+angular.module('starter').service('Analytics', function ($pwaRequest, $q, $rootScope, $log) {
     var service = {};
 
     service.data = {};
 
     service.storeInstallation = function () {
-        if (!Application.is_webview) {
-            var url = Url.get('analytics/mobile_store/installation');
-            var params = {
-                OS: device.platform,
-                OSVersion: device.version,
-                Device: device.platform,
-                DeviceVersion: device.model,
-                deviceUUID: device.uuid,
-                latitude: null,
-                longitude: null
-            };
+        var params = {
+            OS: device.platform,
+            OSVersion: device.version,
+            Device: device.platform,
+            DeviceVersion: device.model,
+            deviceUUID: device.uuid,
+            latitude: null,
+            longitude: null
+        };
 
-            $cordovaGeolocation.getCurrentPosition({
-                enableHighAccuracy: true,
-                timeout: 30000,
-                maximumAge: 0
-            }).then(function (position) {
-                params.latitude = position.coords.latitude;
-                params.longitude = position.coords.longitude;
-
-                service.postData(url, params);
-            }, function () {
-                service.postData(url, params);
-            });
-        }
+        service.postData('analytics/mobile_store/installation', params);
     };
 
     service.storeOpening = function () {
         var deferred = $q.defer();
+        var params = {
+            OS: cordova.device ? device.platform : 'Browser',
+            OSVersion: cordova.device ? device.version : null,
+            Device: cordova.device ? device.platform : 'Browser',
+            DeviceVersion: cordova.device ? device.model : null,
+            deviceUUID: cordova.device ? device.uuid : null,
+            latitude: null,
+            longitude: null,
+            locale: CURRENT_LANGUAGE
+        };
 
-        if (!Application.is_webview && (typeof cordova !== 'undefined')) {
-            var url = Url.get('analytics/mobile_store/opening');
-            var params = {
-                OS: cordova.device ? device.platform : 'Browser',
-                OSVersion: cordova.device ? device.version : null,
-                Device: cordova.device ? device.platform : 'Browser',
-                DeviceVersion: cordova.device ? device.model : null,
-                deviceUUID: cordova.device ? device.uuid : null,
-                latitude: null,
-                longitude: null,
-                locale: CURRENT_LANGUAGE
-            };
-
-            $cordovaGeolocation.getCurrentPosition({
-                enableHighAccuracy: true,
-                timeout: 30000,
-                maximumAge: 0
-            }).then(function (position) {
-                params.latitude = position.coords.latitude;
-                params.longitude = position.coords.longitude;
-
-                service.postData(url, params).then(function (result) {
-                    deferred.resolve(result);
-                }).catch(function (error) {
-
-                });
-            }, function () {
-                service.postData(url, params).then(function (result) {
-                    deferred.resolve(result);
-                }).catch(function (error) {
-
-                });
+        service.postData('analytics/mobile_store/opening', params)
+            .then(function (result) {
+                deferred.resolve(result);
+            }).catch(function (error) {
+                deferred.reject();
             });
-        }
 
         return deferred.promise;
     };
 
     service.storeClosing = function () {
-        if (!$rootScope.isOverview) {
-            var url = Url.get('analytics/mobile_store/closing');
-
-            if (typeof service.data.storeClosingId === 'undefined') {
-                $log.debug('aborting /analytics/mobile_store/closing, no id.');
-                return;
-            }
-
-            var params = {
-                id: service.data.storeClosingId
-            };
-
-            service.postData(url, params);
+        if (typeof service.data.storeClosingId === 'undefined') {
+            $log.debug('aborting /analytics/mobile_store/closing, no id.');
+            return;
         }
+
+        var params = {
+            id: service.data.storeClosingId
+        };
+
+        service.postData('analytics/mobile_store/closing', params);
     };
 
     service.storePageOpening = function (page) {
-        if (!$rootScope.isOverview) {
-            var url = Url.get('analytics/mobile_store/pageopening');
-            var params = {
-                featureId: page.value_id,
-                OS: cordova.device ? device.platform : 'Browser',
-                OSVersion: cordova.device ? device.version : null,
-                Device: cordova.device ? device.platform : 'Browser',
-                DeviceVersion: cordova.device ? device.model : null,
-                deviceUUID: cordova.device ? device.uuid : null,
-                latitude: null,
-                longitude: null,
-                locale: CURRENT_LANGUAGE
-            };
+        var params = {
+            featureId: page.value_id,
+            OS: cordova.device ? device.platform : 'Browser',
+            OSVersion: cordova.device ? device.version : null,
+            Device: cordova.device ? device.platform : 'Browser',
+            DeviceVersion: cordova.device ? device.model : null,
+            deviceUUID: cordova.device ? device.uuid : null,
+            latitude: null,
+            longitude: null,
+            locale: CURRENT_LANGUAGE
+        };
 
-            $cordovaGeolocation.getCurrentPosition({
-                enableHighAccuracy: true,
-                timeout: 30000,
-                maximumAge: 0
-            }).then(function (position) {
-                params.latitude = position.coords.latitude;
-                params.longitude = position.coords.longitude;
-
-                service.postData(url, params);
-            }, function () {
-                service.postData(url, params);
-            });
-        }
+        service.postData('analytics/mobile_store/pageopening', params);
     };
 
     service.storeProductOpening = function (product) {
-        if (!$rootScope.isOverview) {
-            var url = Url.get('analytics/mobile_store/productopening');
-            var params = {
-                productId: product.id,
-                name: product.name,
-                OS: cordova.device ? device.platform : 'Browser',
-                OSVersion: cordova.device ? device.version : null,
-                Device: cordova.device ? device.platform : 'Browser',
-                DeviceVersion: cordova.device ? device.model : null,
-                deviceUUID: cordova.device ? device.uuid : null,
-                latitude: null,
-                longitude: null,
-                locale: CURRENT_LANGUAGE
-            };
+        var params = {
+            productId: product.id,
+            name: product.name,
+            OS: cordova.device ? device.platform : 'Browser',
+            OSVersion: cordova.device ? device.version : null,
+            Device: cordova.device ? device.platform : 'Browser',
+            DeviceVersion: cordova.device ? device.model : null,
+            deviceUUID: cordova.device ? device.uuid : null,
+            latitude: null,
+            longitude: null,
+            locale: CURRENT_LANGUAGE
+        };
 
-            $cordovaGeolocation.getCurrentPosition({
-                enableHighAccuracy: true,
-                timeout: 30000,
-                maximumAge: 0
-            }).then(function (position) {
-                params.latitude = position.coords.latitude;
-                params.longitude = position.coords.longitude;
-
-                service.postData(url, params);
-            }, function () {
-                service.postData(url, params);
-            });
-        }
+        service.postData('analytics/mobile_store/productopening', params);
     };
 
     service.storeProductSold = function (products) {
-        if (!$rootScope.isOverview) {
-            var url = Url.get('analytics/mobile_store/productsold');
-            var params = {
-                products: products,
-                OS: cordova.device ? device.platform : 'Browser',
-                OSVersion: cordova.device ? device.version : null,
-                Device: cordova.device ? device.platform : 'Browser',
-                DeviceVersion: cordova.device ? device.model : null,
-                deviceUUID: cordova.device ? device.uuid : null,
-                latitude: null,
-                longitude: null,
-                locale: CURRENT_LANGUAGE
-            };
+        var params = {
+            products: products,
+            OS: cordova.device ? device.platform : 'Browser',
+            OSVersion: cordova.device ? device.version : null,
+            Device: cordova.device ? device.platform : 'Browser',
+            DeviceVersion: cordova.device ? device.model : null,
+            deviceUUID: cordova.device ? device.uuid : null,
+            latitude: null,
+            longitude: null,
+            locale: CURRENT_LANGUAGE
+        };
 
-            $cordovaGeolocation.getCurrentPosition({
-                enableHighAccuracy: true,
-                timeout: 30000,
-                maximumAge: 0
-            }).then(function (position) {
-                params.latitude = position.coords.latitude;
-                params.longitude = position.coords.longitude;
-
-                service.postData(url, params);
-            }, function () {
-                service.postData(url, params);
-            });
-        }
+        service.postData('analytics/mobile_store/productsold', params);
     };
 
     service.postData = function (url, params) {
-        return $pwaRequest.post(url, {
-            data: params,
-            cache: false,
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            }
-        });
+        if (!isOverview) {
+            return $pwaRequest.post(url, {
+                data: params,
+                cache: false,
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }
+            });
+        }
+        return $pwaRequest.reject('Analytics are disabled in Overview!');
     };
 
     return service;
