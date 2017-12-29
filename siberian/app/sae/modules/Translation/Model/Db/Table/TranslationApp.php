fc7eff08eb53d4560c1376f0ff00d364c743c11d
Alex-Yves CREPIN <a.crepin@xtraball.com>
2017-12-29 14:37:05 +0100

    feat: translations

---
+++ siberian/app/sae/modules/Translation/Model/Db/Table/TranslationApp.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Class Translation_Model_Db_Table_TranslationApp
+ */
+class Translation_Model_Db_Table_TranslationApp extends Core_Model_Db_Table {
+
+    /**
+     * @var string
+     */
+    protected $_name = 'translation_app';
+
+    /**
+     * @var string
+     */
+    protected $_primary = "translation_id";
+
+}
