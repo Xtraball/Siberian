87f1a8343b667598959f5f7641564a8f16af5318
Alex-Yves CREPIN <a.crepin@xtraball.com>
2018-01-09 14:09:32 +0100

    chore: prepped platforms

--- ionic/platforms/ios/www/js/factory/set-meal.js
+++ ionic/platforms/ios/www/js/factory/set-meal.js
@@ -1,43 +1,42 @@
-/*global
- App, device, angular
- */
-
 /**
- * SetMeal
+ * Set Meal
  *
  * @author Xtraball SAS
  */
-angular.module("starter").factory("SetMeal", function($pwaRequest) {
-
+angular.module('starter').factory('SetMeal', function ($pwaRequest) {
     var factory = {
-        value_id            : null,
-        set_meal_id         : null,
-        displayed_per_page  : null,
-        extendedOptions     : {},
-        collection          :[]
+        value_id: null,
+        set_meal_id: null,
+        displayed_per_page: null,
+        extendedOptions: {},
+        collection: []
     };
 
     /**
      *
-     * @param value_id
+     * @param valueId
      */
-    factory.setValueId = function(value_id) {
-        factory.value_id = value_id;
+    factory.setValueId = function (valueId) {
+        // Clear collection when changing valueId!
+        if (factory.value_id !== valueId) {
+            factory.collection = [];
+        }
+        factory.value_id = valueId;
     };
 
     /**
      *
-     * @param value_id
+     * @param setMealId
      */
-    factory.setSetMealId = function(set_meal_id) {
-        factory.set_meal_id = set_meal_id;
+    factory.setSetMealId = function (setMealId) {
+        factory.set_meal_id = setMealId;
     };
 
     /**
      *
      * @param options
      */
-    factory.setExtendedOptions = function(options) {
+    factory.setExtendedOptions = function (options) {
         factory.extendedOptions = options;
     };
 
@@ -46,20 +45,24 @@ angular.module("starter").factory("SetMeal", function($pwaRequest) {
      *
      * @param page
      */
-    factory.preFetch = function(page) {
+    factory.preFetch = function (page) {
         factory.findAll();
     };
 
-    factory.findAll = function(offset, refresh) {
-
-        if(!this.value_id) {
-            return $pwaRequest.reject("[Factory::SetMeal.findAll] missing value_id");
+    /**
+     *
+     * @param offset
+     * @param refresh
+     */
+    factory.findAll = function (offset, refresh) {
+        if (!this.value_id) {
+            return $pwaRequest.reject('[Factory::SetMeal.findAll] missing value_id');
         }
 
-        return $pwaRequest.get("catalog/mobile_setmeal_list/findall", angular.extend({
+        return $pwaRequest.get('catalog/mobile_setmeal_list/findall', angular.extend({
             urlParams: {
-                value_id    : this.value_id,
-                offset      : offset
+                value_id: this.value_id,
+                offset: offset
             },
             refresh: refresh
         }, factory.extendedOptions));
@@ -68,22 +71,22 @@ angular.module("starter").factory("SetMeal", function($pwaRequest) {
     /**
      * Fallback for direct call.
      *
-     * @param set_meal_id
+     * @param setMealId
      */
-    factory.find = function(set_meal_id) {
-
-        if(set_meal_id === undefined) {
-            set_meal_id = factory.set_meal_id;
+    factory.find = function (setMealId) {
+        var localSetMealId = setMealId;
+        if (localSetMealId === undefined) {
+            localSetMealId = factory.setMealId;
         }
 
-        if(!this.value_id && !set_meal_id) {
-            return $pwaRequest.reject("[Factory::SetMeal.find] missing value_id or set_meal_id");
+        if (!this.value_id && !localSetMealId) {
+            return $pwaRequest.reject('[Factory::SetMeal.find] missing value_id or set_meal_id');
         }
 
-        return $pwaRequest.get("catalog/mobile_setmeal_view/find", {
+        return $pwaRequest.get('catalog/mobile_setmeal_view/find', {
             urlParams: {
-                value_id        : this.value_id,
-                set_meal_id     : set_meal_id
+                value_id: this.value_id,
+                set_meal_id: localSetMealId
             }
         });
     };
@@ -91,27 +94,22 @@ angular.module("starter").factory("SetMeal", function($pwaRequest) {
     /**
      * Search for set meal payload inside cached collection
      *
-     * @param set_meal_id
      * @returns {*}
      */
-    factory.getSetMeal = function() {
-
-        if(!this.value_id) {
-            return $pwaRequest.reject("[Factory::SetMeal.getSetMeal] missing value_id");
+    factory.getSetMeal = function () {
+        if (!this.value_id) {
+            return $pwaRequest.reject('[Factory::SetMeal.getSetMeal] missing value_id');
         }
 
-        var set_meal = _.get(_.filter(factory.collection, function(set_meal) {
-            return (set_meal.id == factory.set_meal_id);
-        })[0], "embed_payload", false);
+        var set_meal = _.get(_.filter(factory.collection, function (filter_set_meal) {
+            return (filter_set_meal.id == factory.set_meal_id);
+        })[0], 'embed_payload', false);
 
-        if(!set_meal) {
-            /** Well then fetch it. */
+        if (!set_meal) {
+            // Well then fetch it!
             return factory.find();
-
-        } else {
-
-            return $pwaRequest.resolve(set_meal);
         }
+        return $pwaRequest.resolve(set_meal);
     };
 
     return factory;
