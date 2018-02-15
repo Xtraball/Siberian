ed1489e4ca70c0573179815c818ad7a2f95f2ed5
Alex-Yves CREPIN <a.crepin@xtraball.com>
2018-02-15 11:30:25 +0100

    fix: background-images not being deleted, and replaced by default placeholder.

--- siberian/app/sae/modules/Application/controllers/Customization/Features/Edit/BackgroundController.php
+++ siberian/app/sae/modules/Application/controllers/Customization/Features/Edit/BackgroundController.php
@@ -1,15 +1,20 @@
 <?php
 
+/**
+ * Class Application_Customization_Features_Edit_BackgroundController
+ */
 class Application_Customization_Features_Edit_BackgroundController extends Application_Controller_Default {
 
     /**
      * @var array
      */
-    public $cache_triggers = array(
-        "save" => array(
-            "tags" => array("app_#APP_ID#"),
-        ),
-    );
+    public $cache_triggers = [
+        'save' => [
+            'tags' => [
+                'app_#APP_ID#'
+            ],
+        ],
+    ];
 
     /**
      * Simple edit post, validator
@@ -18,13 +23,13 @@ class Application_Customization_Features_Edit_BackgroundController extends Appli
         $values = $this->getRequest()->getPost();
 
         $form = new Application_Form_BackgroundImage();
-        if($form->isValid($values)) {
-            /** Do whatever you need when form is valid */
+        if ($form->isValid($values)) {
+            // Do whatever you need when form is valid!
             $option_value = $this->getCurrentOptionValue();
 
-            if($values['background_image'] == '_delete_') {
-                $option_value->setData('background_image', '"');
-            } else if(file_exists(Core_Model_Directory::getBasePathTo("images/application" . $values['background_image']))) {
+            if ($values['background_image'] == '_delete_') {
+                $option_value->setData('background_image', '');
+            } else if (file_exists(Core_Model_Directory::getBasePathTo("images/application" . $values['background_image']))) {
                 # Nothing changed, skip
             } else {
                 $background = Siberian_Feature::moveUploadedFile($this->getCurrentOptionValue(),
@@ -32,9 +37,9 @@ class Application_Customization_Features_Edit_BackgroundController extends Appli
                 $option_value->setData('background_image', $background);
             }
 
-            if($values['background_landscape_image'] == '_delete_') {
-                $option_value->setData('background_landscape_image', '"');
-            } else if(file_exists(Core_Model_Directory::getBasePathTo("images/application" . $values['background_landscape_image']))) {
+            if ($values['background_landscape_image'] == '_delete_') {
+                $option_value->setData('background_landscape_image', '');
+            } else if (file_exists(Core_Model_Directory::getBasePathTo("images/application" . $values['background_landscape_image']))) {
                 # Nothing changed, skip
             } else {
                 $background = Siberian_Feature::moveUploadedFile($this->getCurrentOptionValue(),
@@ -49,17 +54,16 @@ class Application_Customization_Features_Edit_BackgroundController extends Appli
                 ->touch()
                 ->expires(-1);
 
-            $payload = array(
-                'success' => 1,
+            $payload = [
+                'success' => true,
                 'message' => __('Success.'),
-            );
+            ];
         } else {
-            /** Do whatever you need when form is not valid */
-            $payload = array(
-                'error' => 1,
+            $payload = [
+                'error' => true,
                 'message' => $form->getTextErrors(),
                 'errors' => $form->getTextErrors(true)
-            );
+            ];
         }
 
         $this->_sendJson($payload);
