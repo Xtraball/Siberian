4dfef75603e427a8d8af83d3c3f9cf87d0cd867d
Alex-Yves CREPIN <a.crepin@xtraball.com>
2018-04-23 16:37:47 +0200

    fix: template/crud builder fixes

--- ci/scripts/modules_templates/Form/form_delete.php
+++ ci/scripts/modules_templates/Form/form_delete.php
@@ -3,16 +3,19 @@
 /**
  * Class #MODULE#_Form_#MODEL#_Delete
  */
-class #MODULE#_Form_#MODEL#_Delete extends Siberian_Form_Abstract {
-
-    public function init() {
+class #MODULE#_Form_#MODEL#_Delete extends Siberian_Form_Abstract
+{
+    /**
+     * init wrapper
+     */
+    public function init()
+    {
         parent::init();
 
         $this
             ->setAction(__path("#FORM_DELETE_ACTION#"))
             ->setAttrib("id", "form-delete-#FORM_ID#")
             ->setConfirmText("You are about to remove this #HUMAN# ! Are you sure ?");
-        ;
 
         /** Bind as a delete form */
         self::addClass("delete", $this);
@@ -20,8 +23,7 @@ class #MODULE#_Form_#MODEL#_Delete extends Siberian_Form_Abstract {
         $db = Zend_Db_Table::getDefaultAdapter();
         $select = $db->select()
             ->from('#TABLE_NAME#')
-            ->where('#TABLE_NAME#.#PRIMARY_KEY# = :value')
-        ;
+            ->where('#TABLE_NAME#.#PRIMARY_KEY# = :value');
 
         $#PRIMARY_KEY# = $this->addSimpleHidden("#PRIMARY_KEY#", __("#MODEL#"));
         $#PRIMARY_KEY#->addValidator("Db_RecordExists", true, $select);
