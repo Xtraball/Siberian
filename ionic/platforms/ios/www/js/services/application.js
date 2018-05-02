0eb2027f61a5943a2fc579197ad48ada16df44c1
Alex-Yves CREPIN <a.crepin@xtraball.com>
2018-05-02 12:01:20 +0200

    chore: prepped platforms

--- ionic/platforms/ios/www/js/services/application.js
+++ ionic/platforms/ios/www/js/services/application.js
@@ -141,7 +141,7 @@ angular.module('starter').service('Application', function ($pwaRequest, $q, $roo
         service.app_id = data.application.id;
         service.app_name = data.application.name;
         service.privacy_policy = data.application.privacy_policy;
-        service.privacyPolicyGdpr = data.application.privacyPolicyGdpr;
+        service.privacyPolicyGdpr = data.application.privacy_policy_gdpr;
         service.gdprIsEnabled = data.application.gdprIsEnabled;
         service.privacy_policy_title = data.application.privacy_policy_title;
         service.googlemaps_key = data.application.googlemaps_key;
