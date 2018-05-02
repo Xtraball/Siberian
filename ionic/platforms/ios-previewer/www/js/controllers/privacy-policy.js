21688e4c6276b13f72f7794d5c18f55caef01047
Alex-Yves CREPIN <a.crepin@xtraball.com>
2018-05-02 14:22:28 +0200

    chore: prepped platforms

--- ionic/platforms/ios-previewer/www/js/controllers/privacy-policy.js
+++ ionic/platforms/ios-previewer/www/js/controllers/privacy-policy.js
@@ -6,7 +6,7 @@ angular.module('starter').controller('PrivacyPolicyController', function($scope,
         value_id: $stateParams.value_id,
         page_title: Application.privacy_policy_title,
         privacy_policy: Application.privacy_policy,
-        privacyPolicyGdpr: Application.privacy_policy_gdpr,
+        privacy_policy_gdpr: Application.privacy_policy_gdpr,
         card_design: false,
         gdpr: {
             isEnabled: Application.gdprIsEnabled
