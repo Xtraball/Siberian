6923bea1e3f67074579152125f68227ce442ff41
Alex-Yves CREPIN <a.crepin@xtraball.com>
2018-05-15 17:59:30 +0200

    fix: calendar event dates using bad timezone.

--- siberian/lib/Siberian/Date.php
+++ siberian/lib/Siberian/Date.php
@@ -1,35 +1,76 @@
 <?php
 
-class Siberian_Date extends Zend_Date {
-
+/**
+ * Class Siberian_Date
+ */
+class Siberian_Date extends Zend_Date
+{
+    /**
+     * @var integer
+     */
     const HOUR_SECONDS = 3600;
+
+    /**
+     * @var integer
+     */
     const DAY_SECONDS = 86400;
+
+    /**
+     * @var integer
+     */
     const WEEK_SECONDS = 604800;
 
-    public static function now($locale = null) {
+    /**
+     * @param null $locale
+     * @return Siberian_Date|Zend_Date
+     * @throws Zend_Date_Exception
+     */
+    public static function now($locale = null)
+    {
         return new self(time(), self::TIMESTAMP, $locale);
     }
 
+    /**
+     * @return $this
+     * @throws Zend_Date_Exception
+     */
     public function toGmt() {
         $this->addSecond($this->getGmtOffset());
         return $this;
     }
 
-    public function setBeginningOfTheDay() {
+    /**
+     * @return $this
+     * @throws Zend_Date_Exception
+     */
+    public function setBeginningOfTheDay()
+    {
         $this->setHour(0);
         $this->setMinute(0);
         $this->setSecond(0);
         return $this;
     }
 
-    public function setEndOfTheDay() {
+    /**
+     * @return $this
+     * @throws Zend_Date_Exception
+     */
+    public function setEndOfTheDay()
+    {
         $this->setHour(23);
         $this->setMinute(59);
         $this->setSecond(59);
         return $this;
     }
 
-    public static function format($date = null, $format = 'y-MM-dd') {
+    /**
+     * @param null $date
+     * @param string $format
+     * @return string
+     * @throws Zend_Date_Exception
+     */
+    public static function format($date = null, $format = 'y-MM-dd')
+    {
         $date = new Zend_Date($date, 'y-MM-dd HH:mm:ss');
         return $date->toString($format);
     }
