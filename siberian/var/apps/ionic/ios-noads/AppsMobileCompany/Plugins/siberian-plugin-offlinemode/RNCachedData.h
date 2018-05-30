d8835109f819bf4527e84243290bf8c943fd2580
Alex-Yves CREPIN <a.crepin@xtraball.com>
2018-05-30 19:08:46 +0200

    chore: clean-up ios-noads

--- siberian/var/apps/ionic/ios-noads/AppsMobileCompany/Plugins/siberian-plugin-offlinemode/RNCachedData.h
+++
@@ -1,8 +0,0 @@
-static NSString *RNCachingURLHeader = @"X-RNCache";
-
-
-@interface RNCachedData : NSObject <NSCoding>
-@property (nonatomic, readwrite, strong) NSData *data;
-@property (nonatomic, readwrite, strong) NSURLResponse *response;
-@property (nonatomic, readwrite, strong) NSURLRequest *redirectRequest;
-@end
