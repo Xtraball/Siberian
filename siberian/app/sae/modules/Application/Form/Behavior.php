5581e922d82425e6b6ca0ca67eeebc5e23cc2378
Alex-Yves CREPIN <a.crepin@xtraball.com>
2018-05-30 18:32:16 +0200

    feat: add hints for statusbar options ios/android

--- siberian/app/sae/modules/Application/Form/Behavior.php
+++ siberian/app/sae/modules/Application/Form/Behavior.php
@@ -25,9 +25,29 @@ class Application_Form_Behavior extends Siberian_Form_Abstract {
         $ios_status->setCols("col-md-10 col-xs-10", "col-md-1 col-xs-1", "");
         $ios_status->setNewDesign();
 
-        $android_status = $this->addSimpleCheckbox("android_status_bar_is_hidden", __("Hide Android status bar"));
+        $iosStatus = '<span style="font-style: italic; font-weight: bold; font-size: 11px;">' .
+            __('Changing this option requires you to republish your iOS application.') .
+            '</span>';
+        $this->addSimpleHtml('ios_status', $iosStatus,
+            [
+                'class' => 'col-md-10 col-xs-10',
+                'style' => 'margin-top: -20px;'
+            ]);
+
+        $android_status = $this->addSimpleCheckbox(
+            "android_status_bar_is_hidden",
+            __("Hide Android status bar"));
         $android_status->setCols("col-md-10 col-xs-10", "col-md-1 col-xs-1", "");
         $android_status->setNewDesign();
+
+        $androidStatus = '<span style="font-style: italic; font-weight: bold; font-size: 11px;">' .
+            __('This option is not compatible with applications using Forms and/or features with Keyboard.') .
+            '</span>';
+        $this->addSimpleHtml('android_status', $androidStatus,
+            [
+                'class' => 'col-md-10 col-xs-10',
+                'style' => 'margin-top: -20px;'
+            ]);
 
     }
 }
