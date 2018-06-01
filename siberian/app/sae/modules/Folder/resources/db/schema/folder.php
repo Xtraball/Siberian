e6f76c3e182c0ad6ffdb909dec6996129bd97fbc
Alex-Yves CREPIN <a.crepin@xtraball.com>
2018-06-01 15:38:02 +0200

    feat: adds line_return option to table

--- siberian/app/sae/modules/Folder/resources/db/schema/folder.php
+++ siberian/app/sae/modules/Folder/resources/db/schema/folder.php
@@ -6,59 +6,59 @@
  * Last update: 2016-04-28
  *
  */
-$schemas = (!isset($schemas)) ? [] : $schemas;
-$schemas['folder'] = [
-    'folder_id' => [
+$schemas = (!isset($schemas)) ? array() : $schemas;
+$schemas['folder'] = array(
+    'folder_id' => array(
         'type' => 'int(11) unsigned',
         'auto_increment' => true,
         'primary' => true,
-    ],
-    'value_id' => [
+    ),
+    'value_id' => array(
         'type' => 'int(11) unsigned',
-        'foreign_key' => [
+        'foreign_key' => array(
             'table' => 'application_option_value',
             'column' => 'value_id',
             'name' => 'folder_ibfk_1',
             'on_update' => 'CASCADE',
             'on_delete' => 'CASCADE',
-        ],
-        'index' => [
+        ),
+        'index' => array(
             'key_name' => 'KEY_VALUE_ID',
             'index_type' => 'BTREE',
             'is_null' => false,
             'is_unique' => false,
-        ],
-    ],
-    'root_category_id' => [
+        ),
+    ),
+    'root_category_id' => array(
         'type' => 'int(11) unsigned',
-        'foreign_key' => [
+        'foreign_key' => array(
             'table' => 'folder_category',
             'column' => 'category_id',
             'name' => 'folder_ibfk_2',
             'on_update' => 'CASCADE',
             'on_delete' => 'CASCADE',
-        ],
-        'index' => [
+        ),
+        'index' => array(
             'key_name' => 'KEY_CAT_ID',
             'index_type' => 'BTREE',
             'is_null' => false,
             'is_unique' => false,
-        ],
-    ],
-    'version' => [
+        ),
+    ),
+    'version' => array(
         'type' => 'int(1)',
         'default' => '1',
-    ],
-    'show_search' => [
+    ),
+    'show_search' => array(
         'type' => 'tinyint(1) unsigned',
         'default' => '0',
-    ],
-    'card_design' => [
+    ),
+    'card_design' => array(
         'type' => 'tinyint(1) unsigned',
         'default' => '0',
-    ],
-    'allow_line_return' => [
+    ),
+    'allow_line_return' => array(
         'type' => 'tinyint(1) unsigned',
         'default' => '0',
-    ]
-];
+    )
+);
