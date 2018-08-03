3ebf0a68048c63f3ae704256ff0c5d24ce85e200
Alex-Yves CREPIN <a.crepin@xtraball.com>
2018-08-03 13:57:09 +0200

    feat(plesk): revert version changes for API.

--- siberian/lib/Plesk/SSL/DeleteCertificate.php
+++ siberian/lib/Plesk/SSL/DeleteCertificate.php
@@ -14,7 +14,7 @@ class DeleteCertificate extends BaseRequest
      */
     public $xml_packet = <<<EOT
 <?xml version="1.0"?>
-<packet version="1.6.9.1">
+<packet version="1.6.3.0">
     <certificate>
         <remove>
             <filter>
--- siberian/lib/Plesk/UpdateClient.php
+++ siberian/lib/Plesk/UpdateClient.php
@@ -8,7 +8,7 @@ class UpdateClient extends BaseRequest
      */
     public $xml_packet = <<<EOT
 <?xml version="1.0"?>
-<packet version="1.6.9.1">
+<packet version="1.6.3.0">
     <customer>
         <set>
             <filter>
