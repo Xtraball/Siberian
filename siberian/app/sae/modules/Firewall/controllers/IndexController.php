abb5dd9410d131f547555429b65a0ab9a6ceb8c1
Alex-Yves CREPIN <anders@xtraball.com>
2018-08-22 15:44:04 +0200

    fix(clamav): settings, message file upload form

--- siberian/app/sae/modules/Firewall/controllers/IndexController.php
+++ siberian/app/sae/modules/Firewall/controllers/IndexController.php
@@ -15,6 +15,11 @@ class Firewall_IndexController extends Backoffice_Controller_Default
         $payload = [
             'title' => __('Advanced') . ' > ' . __('Firewall'),
             'icon' => 'icofont icofont-ui-fire-wall',
+            'fw_clamd' => [
+                //'sock' => __get('fw_clamd_sock'),
+                'ip' => __get('fw_clamd_ip'),
+                'port' => __get('fw_clamd_port'),
+            ],
         ];
 
         $rules = (new \Firewall_Model_Rule())
@@ -113,6 +118,45 @@ class Firewall_IndexController extends Backoffice_Controller_Default
             $payload = [
                 'success' => true,
                 'message' => __('Rule have been added.'),
+            ];
+
+        } catch (\Exception $e) {
+            $payload = [
+                'error' => true,
+                'message' => $e->getMessage(),
+            ];
+        }
+
+        $this->_sendJson($payload);
+    }
+
+    public function savefwclamdsettingsAction()
+    {
+        try {
+            $request = $this->getRequest();
+            $params = $request->getBodyParams();
+
+            if (empty($params)) {
+                throw new \Siberian\Exception(__('Missing values'));
+            }
+
+            //$sock = $params['fw_clamd_sock'];
+            $ip = $params['fw_clamd_ip'];
+            $port = $params['fw_clamd_port'];
+
+            if (empty($ip) &&
+                empty($port)) {
+                throw new \Siberian\Exception(__("You must fill ip & port."));
+            }
+
+            // Saving values
+            //__set('fw_clamd_sock', $sock);
+            __set('fw_clamd_ip', $ip);
+            __set('fw_clamd_port', $port);
+
+            $payload = [
+                'success' => true,
+                'message' => __('ClamAV settings have been saved.'),
             ];
 
         } catch (\Exception $e) {
