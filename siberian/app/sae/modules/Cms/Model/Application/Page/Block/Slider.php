e4441ce1f3d4b0331650df7c46e0e289a4f9eef9
Alex-Yves CREPIN <anders@xtraball.com>
2018-08-24 16:06:03 +0200

    feat(cms): line return options for slider

--- siberian/app/sae/modules/Cms/Model/Application/Page/Block/Slider.php
+++ siberian/app/sae/modules/Cms/Model/Application/Page/Block/Slider.php
@@ -3,27 +3,42 @@
 /**
  * Class Cms_Model_Application_Page_Block_Slider
  */
-class Cms_Model_Application_Page_Block_Slider extends Cms_Model_Application_Page_Block_Image_Abstract {
-
+class Cms_Model_Application_Page_Block_Slider extends Cms_Model_Application_Page_Block_Image_Abstract
+{
     /**
      * Cms_Model_Application_Page_Block_Slider constructor.
      * @param array $params
+     * @throws Zend_Exception
      */
-    public function __construct($params = array()) {
+    public function __construct($params = [])
+    {
         parent::__construct($params);
         $this->_db_table = 'Cms_Model_Db_Table_Application_Page_Block_Slider';
         return $this;
     }
 
     /**
-     * @return bool
+     * @return bool|mixed
      */
-    public function isValid() {
-        if($this->getLibraryId()) {
+    public function isValid()
+    {
+        if ($this->getLibraryId()) {
             return true;
         }
 
         return false;
     }
-    
+
+    /**
+     * @param array $data
+     * @return $this
+     */
+    public function populate($data = []) {
+        $this->setAllowLineReturn($data['allow_line_return']);
+
+        parent::populate($data);
+
+        return $this;
+    }
+
 }
