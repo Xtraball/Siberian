0e8c69f4af8e62db366498f94240dc48fbd6730a
Alex-Yves CREPIN <anders@xtraball.com>
2018-11-28 15:56:21 +0100

    feat: improves folders v2 multi-terms search

--- ionic/platforms/browser/www/js/providers/homepage-layout.js
+++ ionic/platforms/browser/www/js/providers/homepage-layout.js
@@ -14,9 +14,6 @@ angular.module('starter').provider('HomepageLayout', function () {
 
                 if (currentPage.code === "folder_v2") {
                     currentPage.embed_payload.collection.forEach(function (subPage) {
-                        console.log("valueId", valueId);
-                        console.log("subPage.value_id", subPage.value_id);
-                        console.log("subPage", subPage);
                         if (subPage.value_id == valueId) {
                             layoutId = subPage.layout_id;
                             return layoutId;
