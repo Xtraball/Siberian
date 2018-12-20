b4082254b71cc1d96ef090e46c0d1ae5f3984aa5
Alex-Yves CREPIN <anders@xtraball.com>
2018-12-20 16:53:41 +0100

    fix: gzip fonts.

--- siberian/app/sae/modules/Template/Model/Design.php
+++ siberian/app/sae/modules/Template/Model/Design.php
@@ -180,11 +180,7 @@ class Template_Model_Design extends Core_Model_Default
                 "family" => $fontFamily,
                 "subset" => "latin,greek,cyrillic",
             ], null, null, [
-                "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.80 Safari/537.36",
-                "Accept-Encoding: gzip, deflate, br",
-                "Accept-Language: *",
-                "Pragma: No-Cache",
-                "Cache-Control: No-Cache"
+                "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.80 Safari/537.36"
             ]);
 
             if (\Siberian_Request::$statusCode == 200) {
