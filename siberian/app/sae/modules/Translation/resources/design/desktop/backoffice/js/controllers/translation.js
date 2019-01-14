b9b63655fb6f188bcbf84960c47d3146a90c2a30
Alex-Yves CREPIN <anders@xtraball.com>
2019-01-14 15:30:13 +0100

    fix: unique filter for records.

--- siberian/app/sae/modules/Translation/resources/design/desktop/backoffice/js/controllers/translation.js
+++ siberian/app/sae/modules/Translation/resources/design/desktop/backoffice/js/controllers/translation.js
@@ -337,6 +337,37 @@ App.filter('multiWordsFilter', function($filter) {
             }
         });
 
+        if (result.length > 0) {
+            result = $filter('repeatUnique')(result, 'search')
+        }
+
         return result;
+    };
+}).filter('repeatUnique', function() {
+    // we will return a function which will take in a collection
+    // and a keyname
+    return function(collection, keyName) {
+        // we define our output and keys array;
+        var output = [],
+            keys = [];
+
+        // we utilize angular's foreach function
+        // this takes in our original collection and an iterator function
+        angular.forEach(collection, function(item) {
+            if (item) {
+                // we check to see whether our object exists
+                var key = item[keyName];
+                // if it's not already part of our keys array
+                if(keys.indexOf(key) === -1) {
+                    // add it to our keys array
+                    keys.push(key);
+                    // push this item to our final output array
+                    output.push(item);
+                }
+            }
+        });
+        // return our array which should be devoid of
+        // any duplicates
+        return output;
     };
 });
