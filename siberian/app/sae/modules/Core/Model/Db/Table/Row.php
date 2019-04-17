7a8f1ff83aa6be89192655024f57aa848ab1305c
Alex-Yves CREPIN <anders@xtraball.com>
2019-04-17 15:27:11 +0200

    feat: timezone translation & created_at / updated_at changes.

--- siberian/app/sae/modules/Core/Model/Db/Table/Row.php
+++ siberian/app/sae/modules/Core/Model/Db/Table/Row.php
@@ -1,113 +1,176 @@
 <?php
 
+/**
+ * Class Core_Model_Db_Table_Row
+ */
 class Core_Model_Db_Table_Row extends Zend_Db_Table_Row_Abstract
 {
 
+    /**
+     * @var
+     */
     protected $_modelClass;
 
+    /**
+     * @var mixed
+     */
     protected $_cols;
 
-    public function __construct(array $options = array()) {
+    /**
+     * Core_Model_Db_Table_Row constructor.
+     * @param array $options
+     * @throws Zend_Db_Table_Row_Exception
+     */
+    public function __construct(array $options = array())
+    {
         parent::__construct($options);
         try {
             $this->_cols = $this->getTable()->info('cols');
-            foreach($this->_cols as $col) {
-                if(!isset($this->_data[$col])) $this->_data[$col] = null;
+            foreach ($this->_cols as $col) {
+                if (!isset($this->_data[$col])) $this->_data[$col] = null;
             }
-        }
-        catch(Exception $e) {
+        } catch (Exception $e) {
 
         }
     }
 
-    public function getModelClass() {
+    /**
+     * @return mixed
+     */
+    public function getModelClass()
+    {
         return $this->_modelClass;
     }
 
-    public function setModelClass($model) {
+    /**
+     * @param $model
+     * @return $this
+     */
+    public function setModelClass($model)
+    {
         $this->_modelClass = $model;
         return $this;
     }
 
-    public function clean() {
+    /**
+     *
+     */
+    public function clean()
+    {
         $this->_data = array();
         $this->_modifiedFields = array();
     }
 
+    /**
+     * @param string $method
+     * @param array $args
+     * @return $this|array|bool|null|Zend_Db_Table_Row_Abstract|Zend_Db_Table_Rowset_Abstract
+     * @throws Zend_Db_Table_Row_Exception
+     */
     public function __call($method, array $args)
     {
         switch (substr($method, 0, 3)) {
             case 'get' :
-                $key = $this->_transformColumn(substr($method,3));
+                $key = $this->_transformColumn(substr($method, 3));
                 return $this->getData($key);
             case 'set' :
-                parent::__set(substr($method,3), isset($args[0]) ? $args[0] : null);
+                parent::__set(substr($method, 3), isset($args[0]) ? $args[0] : null);
                 return $this;
             case 'uns' :
-                $key = $this->_transformColumn(substr($method,3));
+                $key = $this->_transformColumn(substr($method, 3));
                 unset($this->_data[$key]);
                 unset($this->_modifiedFields[$key]);
                 return $this;
             case 'has' :
-                $key = $this->_transformColumn(substr($method,3));
+                $key = $this->_transformColumn(substr($method, 3));
                 return isset($this->_data[$key]);
         }
         return parent::__call($method, $args);
     }
 
-    public function getData($key = null) {
-        if(is_null($key)) return $this->_data;
-        elseif(isset($this->_data[$key])) return $this->_data[$key];
+    /**
+     * @param null $key
+     * @return array|null
+     */
+    public function getData($key = null)
+    {
+        if (is_null($key)) return $this->_data;
+        elseif (isset($this->_data[$key])) return $this->_data[$key];
         else return null;
     }
 
-    public function setData($key, $value = null) {
+    /**
+     * @param $key
+     * @param null $value
+     * @return $this|Core_Model_Db_Table_Row
+     * @throws Zend_Db_Table_Row_Exception
+     */
+    public function setData($key, $value = null)
+    {
 
-        if(is_null($value) AND is_array($key)) return $this->mergeData($key);
+        if (is_null($value) AND is_array($key)) return $this->mergeData($key);
 
         parent::__set($key, $value);
         return $this;
 
     }
 
-    public function mergeData($datas) {
+    /**
+     * @param $datas
+     * @return $this
+     * @throws Exception
+     * @throws Zend_Db_Table_Row_Exception
+     */
+    public function mergeData($datas)
+    {
 
-        foreach($datas as $name => $value) {
-            if($name == 'id' AND !empty($value)) $this->setId(intval($value));
-            if(in_array($name, $this->_cols)) {
+        foreach ($datas as $name => $value) {
+            if ($name == 'id' AND !empty($value)) $this->setId(intval($value));
+            if (in_array($name, $this->_cols)) {
                 $this->setData($name, $value);
             }
         }
 
-        if($this->getId() AND !$this->_isExcluded()) {
+        if ($this->getId() AND !$this->_isExcluded()) {
             $this->_cleanData = $this->_data;
         }
 
         return $this;
     }
 
-    public function getId() {
+    /**
+     * @return array|null|string
+     * @throws Zend_Db_Table_Row_Exception
+     */
+    public function getId()
+    {
 
-        if(!is_array($this->_primary)) return null;
-        if($pks = $this->_getPrimaryKey()) {
-            if(is_array($pks)) return implode('-', $pks);
+        if (!is_array($this->_primary)) return null;
+        if ($pks = $this->_getPrimaryKey()) {
+            if (is_array($pks)) return implode('-', $pks);
             else return $pks;
         }
 
         return null;
     }
 
-    public function setId($id) {
+    /**
+     * @param $id
+     * @return $this|bool
+     * @throws Exception
+     */
+    public function setId($id)
+    {
 
-        if(!is_int($id)) return false;
+        if (!is_int($id)) return false;
         $primaryIsString = (is_string($this->_primary) || is_numeric($this->_primary)) || is_array($this->_primary) && count($this->_primary) == 1;
 
-        if(($primaryIsString && is_array($id) && count($id) > 1) || (!$primaryIsString && count($this->_primary) != count($id))) {
+        if (($primaryIsString && is_array($id) && count($id) > 1) || (!$primaryIsString && count($this->_primary) != count($id))) {
             throw new Exception('Invalid data for id');
         }
 
-        foreach($this->_primary as $pk) {
-            if(is_array($id) && !isset($id[$pk])) {
+        foreach ($this->_primary as $pk) {
+            if (is_array($id) && !isset($id[$pk])) {
                 throw new Exception('Invalid data for id');
             }
             $data = is_array($id) ? $id[$pk] : $id;
@@ -117,40 +180,60 @@ class Core_Model_Db_Table_Row extends Zend_Db_Table_Row_Abstract
         return $this;
     }
 
-    protected function _insert() {
-        $date = new Zend_Date();
-        $date_string = $date->toString('y-MM-dd HH:mm:ss');
-        if(in_array('created_at', $this->_cols) AND !$this->getId()) $this->setCreatedAt($date_string);
-        if(in_array('updated_at', $this->_cols)) $this->setUpdatedAt($date_string);
+    /**
+     * @return $this
+     * @throws Zend_Db_Table_Row_Exception
+     */
+    protected function _insert()
+    {
+        $now = time();
+        $dateString = date("Y-m-d H:i:s", $now);
+        if (in_array('created_at', $this->_cols) && !$this->getId()) {
+            $this->setCreatedAt($dateString);
+        }
+        if (in_array('updated_at', $this->_cols)) {
+            $this->setUpdatedAt($dateString);
+        }
 
-        if(in_array('updated_at_utc', $this->_cols)) $this->setUpdatedAtUtc($date->getTimestamp());
+        if (in_array('updated_at_utc', $this->_cols)) {
+            $this->setUpdatedAtUtc($now);
+        }
 
-        if(in_array('created_at_utc', $this->_cols) AND !$this->getCreatedAtUtc()) {
-            $created_at = $this->getId() ? new Zend_Date($this->getCreatedAt()) : $date;
-            $this->setCreatedAtUtc($created_at->getTimestamp());
+        if (in_array('created_at_utc', $this->_cols) && !$this->getCreatedAtUtc()) {
+            $this->setCreatedAtUtc($now);
         }
 
         return $this;
     }
 
-    protected function _update() {
+    /**
+     * @return $this
+     */
+    protected function _update()
+    {
+        $now = time();
+        $dateString = date("Y-m-d H:i:s", $now);
 
-        $date = new Zend_Date();
-        $date_string = $date->toString('y-MM-dd HH:mm:ss');
-
-        if(in_array('updated_at', $this->_cols)) $this->setUpdatedAt($date_string);
-        if(in_array('updated_at_utc', $this->_cols)) $this->setUpdatedAtUtc($date->getTimestamp());
+        if (in_array('updated_at', $this->_cols)) {
+            $this->setUpdatedAt($dateString);
+        }
+        if (in_array('updated_at_utc', $this->_cols)) {
+            $this->setUpdatedAtUtc($now);
+        }
 
         // Fix created at utc if column is empty
-        if(in_array('created_at_utc', $this->_cols) AND !$this->getCreatedAtUtc()) {
-            $created_at = new Zend_Date($this->getCreatedAt());
-            $this->setCreatedAtUtc($created_at->getTimestamp());
+        if (in_array('created_at_utc', $this->_cols) && !$this->getCreatedAtUtc()) {
+            $this->setCreatedAtUtc($now);
         }
 
         return $this;
     }
 
-    protected function _isExcluded() {
+    /**
+     * @return bool
+     */
+    protected function _isExcluded()
+    {
         return in_array($this->_table->getModelClass(), array(
             'Media_Model_Gallery_Video_Youtube',
             'Media_Model_Gallery_Image_Picasa',
@@ -164,8 +247,13 @@ class Core_Model_Db_Table_Row extends Zend_Db_Table_Row_Abstract
         ));
     }
 
-    protected function _transformColumn($key) {
-        $key = preg_replace('/(?!^)[[:upper:]]/',' \0',$key);
+    /**
+     * @param string $key
+     * @return null|string|string[]
+     */
+    protected function _transformColumn($key)
+    {
+        $key = preg_replace('/(?!^)[[:upper:]]/', ' \0', $key);
         $key = strtolower(str_replace(' ', '_', $key));
         return $key;
     }
