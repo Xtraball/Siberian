4499d11ebf83ed94cc23c7dc9a2cf600b3f421c4
Alex-Yves CREPIN <anders@xtraball.com>
2019-06-17 11:10:30 +0200

    feat: backoffice index for sae/mae.

--- siberian/app/sae/design/desktop/backoffice/js/controllers/homepage.js
+++ siberian/app/sae/design/desktop/backoffice/js/controllers/homepage.js
@@ -18,7 +18,7 @@ App.config(function($routeProvider) {
 
     $scope.header = new Header();
     $scope.header.button.left.is_visible = false;
-    $scope.content_loader_is_visible = true;
+    $scope.content_loader_is_visible = false;
     $scope.show_notif = false;
     $scope.show_more_size = false;
     $scope.show_more_push = false;
@@ -26,6 +26,10 @@ App.config(function($routeProvider) {
     $scope.cron_error_show = false;
     $scope.sslLoading = true;
     $scope.sslWarning = {};
+    $scope.isLoading = true;
+    $scope.graphIsLoading = true;
+    $scope.servicesIsLoading = true;
+    $scope.messagesIsLoading = true;
 
     $scope.clear = function(type, message) {
         if(typeof message != "undefined") {
@@ -50,18 +54,6 @@ App.config(function($routeProvider) {
         $scope.show_cron_modal = true;
     };
 
-    Backoffice.loadData().success(function (data) {
-        $scope.header.title = data.title;
-        $scope.header.icon = data.icon;
-        $scope.services = data.services;
-        $scope.libraries = data.libraries;
-        $scope.server_usage = data.server_usage;
-        $scope.extensions = data.extensions;
-        $scope.external_services = data.external_services;
-        $scope.system_diagnostic = data.system_diagnostic;
-        $scope.new_messages = data.unread_messages;
-    });
-
     $scope.sslWarning = function () {
         $scope.sslLoading = true;
         Backoffice
@@ -77,85 +69,155 @@ App.config(function($routeProvider) {
             });
     };
 
-    Backoffice.find().success(function (data) {
+    $scope.loadGraph = function () {
+        $scope.graphIsLoading = true;
 
-        if(data.notif.unread_number > 0) {
-            $scope.unread_messages = data.notif.message;
+        Backoffice
+        .find()
+        .success(function (data) {
 
-            $timeout(function() {
-                $scope.show_notif = true;
-            }, 1000);
+            if (data.notif.unread_number > 0) {
+                $scope.unread_messages = data.notif.message;
 
-            $timeout(function() {
-                $scope.show_notif = false;
-            }, 10000);
-        }
+                $timeout(function () {
+                    $scope.show_notif = true;
+                }, 1000);
 
-        var stats = data.stats;
-
-        $scope.cssStyle = "height:420px; width:1140px;";
-
-        var labels = stats.map(function(stat){return stat[0];});
-        var newUser  = stats.map(function(stat){return stat[1];});
-
-        $scope.graphSeries  = data.stats_labels;
-        $scope.graphLabels  = labels;
-        $scope.graphData = [newUser];
-        var color = [
-            '204,37,41'
-        ];
-
-        $scope.graphDatasetOverride = [
-            {
-                borderColor:'rgba('+color[0]+',1)',
-                backgroundColor:'rgba('+color[0]+',0.4)',
-                pointBorderColor:'rgba('+color[0]+',0.4)',
-                pointBackgroundColor:'rgba('+color[0]+',1)',
-                pointHoverBackgroundColor:'rgba('+color[0]+',1)',
-                pointHoverBorderColor:'rgba('+color[0]+',0.4)',
-                type:'line',
-                fill:false,
-                lineTension: 0,
-                yAxisID: 'new'
+                $timeout(function () {
+                    $scope.show_notif = false;
+                }, 10000);
             }
-        ];
 
-        $scope.graphOptions = {
-            legend: {
-                display: true,
-            },
-            scales: {
-                yAxes: [
-                    {
-                      afterBuildTicks: function(chartElem) {
-                        var ticks = [];
-                        for (var i = 0 ; i < chartElem.ticks.length; i++) {
-                            //if integer
-                            if(chartElem.ticks[i] % 1 === 0) {
-                                ticks.push(chartElem.ticks[i]);
-                            }
+            var stats = data.stats;
+
+            $scope.cssStyle = "height:420px; width:1140px;";
+
+            var labels = stats.map(function (stat) {
+                return stat[0];
+            });
+            var newUser = stats.map(function (stat) {
+                return stat[1];
+            });
+            var newSale = stats.map(function (stat) {
+                return stat[2];
+            });
+            var payment = stats.map(function (stat) {
+                return stat[3];
+            });
+
+            $scope.graphSeries = data.stats_labels;
+            $scope.graphLabels = labels;
+            $scope.graphData = [newUser];
+            var color = [
+                '204,37,41'
+            ];
+
+            $scope.graphDatasetOverride = [
+                {
+                    borderColor: 'rgba(' + color[0] + ',1)',
+                    backgroundColor: 'rgba(' + color[0] + ',0.4)',
+                    pointBorderColor: 'rgba(' + color[0] + ',0.4)',
+                    pointBackgroundColor: 'rgba(' + color[0] + ',1)',
+                    pointHoverBackgroundColor: 'rgba(' + color[0] + ',1)',
+                    pointHoverBorderColor: 'rgba(' + color[0] + ',0.4)',
+                    type: 'line',
+                    fill: false,
+                    lineTension: 0,
+                    yAxisID: 'new'
+                }
+            ];
+
+            $scope.graphOptions = {
+                legend: {
+                    display: true,
+                },
+                scales: {
+                    yAxes: [
+                        {
+                            afterBuildTicks: function (chartElem) {
+                                var ticks = [];
+                                for (var i = 0; i < chartElem.ticks.length; i++) {
+                                    //if integer
+                                    if (chartElem.ticks[i] % 1 === 0) {
+                                        ticks.push(chartElem.ticks[i]);
+                                    }
+                                }
+                                chartElem.ticks = ticks;
+                                if (chartElem.start < 0) {
+                                    chartElem.start = 0;
+                                }
+                            },
+                            max: 100,
+                            id: 'new',
+                            type: 'linear',
+                            display: true,
+                            position: 'left',
+                            beginAtZero: true
                         }
-                        chartElem.ticks = ticks;
-                        if(chartElem.start < 0) {
-                            chartElem.start = 0;
-                        }
-                      },
-                      max:100,
-                      id: 'new',
-                      type: 'linear',
-                      display: true,
-                      position: 'left',
-                      beginAtZero:true
-                    }
-                ]
-            }
-        };
+                    ]
+                }
+            };
 
+        }).finally(function () {
+            $scope.graphIsLoading = false;
+        });
+    };
 
+    $scope.loadExternalServices = function () {
+        $scope.servicesIsLoading = true;
 
-    }).finally(function() {
-        $scope.content_loader_is_visible = false;
-    });
+        Backoffice
+        .loadExternalServices()
+        .success(function (data) {
+            $scope.external_services = data.external_services;
+
+            $scope.servicesIsLoading = false;
+        }).error(function () {
+            $scope.servicesIsLoading = false;
+        });
+    };
+
+    $scope.loadMessages = function () {
+        $scope.messagesIsLoading = true;
+
+        Backoffice
+        .loadMessages()
+        .success(function (data) {
+            $scope.new_messages = data.unread_messages;
+
+            $scope.messagesIsLoading = false;
+        }).error(function () {
+            $scope.messagesIsLoading = false;
+        });
+    };
+
+    $scope.loadContent = function () {
+        $scope.isLoading = true;
+
+        Backoffice
+        .loadData()
+        .success(function (data) {
+            $scope.header.title = data.title;
+            $scope.header.icon = data.icon;
+            $scope.services = data.services;
+            $scope.libraries = data.libraries;
+            $scope.server_usage = data.server_usage;
+            $scope.extensions = data.extensions;
+            $scope.system_diagnostic = data.system_diagnostic;
+
+            $scope.isLoading = false;
+        }).error(function () {
+            $scope.isLoading = false;
+        });
+    };
+
+    $scope.loadContent();
+    $scope.loadMessages();
+    $scope.loadGraph();
+
+    $timeout(function () {
+        $scope.loadExternalServices();
+    }, 1000);
 
     $scope.sslWarning();
 
