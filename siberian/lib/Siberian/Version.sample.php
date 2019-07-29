7d32f054fe05fddb754d3834a43f327ad673ba2b
Alex-Yves CREPIN <anders@xtraball.com>
2019-07-29 14:19:01 +0200

    chore: increase version sample.

--- siberian/lib/Siberian/Version.sample.php
+++ siberian/lib/Siberian/Version.sample.php
@@ -10,7 +10,7 @@ class Version
 {
     const TYPE = 'SAE';
     const NAME = 'Single App Edition';
-    const VERSION = '4.17.4';
+    const VERSION = '4.17.5';
     const NATIVE_VERSION = '11';
     const API_VERSION = '4';
 
