e4d1542209eb7ba2b01325baa9453a37d9978bf5
Alex-Yves CREPIN <a.crepin@xtraball.com>
2019-11-28 16:15:55 +0100

    chore: bumped version sample.

--- siberian/lib/Siberian/Version.sample.php
+++ siberian/lib/Siberian/Version.sample.php
@@ -10,8 +10,8 @@ class Version
 {
     const TYPE = 'SAE';
     const NAME = 'Single App Edition';
-    const VERSION = '4.18.1';
-    const PREVIOUS_VERSION = '4.18.0';
+    const VERSION = '4.18.2';
+    const PREVIOUS_VERSION = '4.18.1';
     const NATIVE_VERSION = '12';
     const API_VERSION = '4';
 
