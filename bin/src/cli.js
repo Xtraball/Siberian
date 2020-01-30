cb85085b56148ed0f04af05d471c6ef1f9527208
Alex-Yves CREPIN <a.crepin@xtraball.com>
2020-01-30 11:46:21 +0100

    chore(sources): ensure chmod are correct, and throw exception otherwise.

--- bin/src/cli.js
+++ bin/src/cli.js
@@ -1592,15 +1592,15 @@ let archiveSources = function () {
     sh.rm('-rf', './ios/www/features/*');
     sh.rm('-rf', './ios/www/modules/*');
     sh.chmod('-R', '777', './android');
-    sh.exec('tar ' + excludes + ' -czf ./android.tgz ./android');
+    sh.exec('tar ' + excludes + ' -p -czf ./android.tgz ./android');
     sh.chmod('-R', '777', './ios');
-    sh.exec('tar ' + excludes + ' -czf ./ios.tgz ./ios');
+    sh.exec('tar ' + excludes + ' -p -czf ./ios.tgz ./ios');
 
     sh.cd(ROOT + '/siberian/var/apps');
     sh.rm('-rf', './browser/features/*');
     sh.rm('-rf', './browser/modules/*');
     sh.chmod('-R', '777', './browser');
-    sh.exec('tar ' + excludes + ' -czf ./browser.tgz ./browser');
+    sh.exec('tar ' + excludes + ' -p -czf ./browser.tgz ./browser');
 
     sprint(clc.green('Archives done!'));
 };
