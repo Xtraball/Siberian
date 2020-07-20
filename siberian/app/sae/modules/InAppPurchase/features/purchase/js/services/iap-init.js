6038c79dd163c80c1bd9f8c983c501bccb48434f
Alex-Yves CREPIN <a.crepin@xtraball.com>
2020-07-20 16:52:21 +0200

    feat: inapppurchase and css fix for landscape.

--- siberian/app/sae/modules/InAppPurchase/features/purchase/js/services/iap-init.js
+++ siberian/app/sae/modules/InAppPurchase/features/purchase/js/services/iap-init.js
@@ -6,16 +6,21 @@
  */
 angular
     .module('starter')
-    .service('IAP_Init', function (IAP_Product) {
+    .service('IAP_Init', function (IAP_Product, SB) {
         var service = {
 
         };
 
         service.onStart = function () {
+            if (SB.DEVICE.TYPE_BROWSER === DEVICE_TYPE) {
+                console.info('IAP[Support]: InAppPurchase are not available on the WebApp/Browser.');
+                return;
+            }
+            // Android/iOS support!
             IAP_Product
                 .all()
                 .then(function (payload) {
-                    payload.collection.each(function (product) {
+                    payload.collection.forEach(function (product) {
                         console.log('IAP[Product]: ', product);
                     });
                 }, function (error) {
