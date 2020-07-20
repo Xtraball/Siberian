d99610af446944c6b46e51d4b2505a2e45eec434
Alex-Yves CREPIN <a.crepin@xtraball.com>
2020-07-20 17:18:46 +0200

    feat: adds user_id on exists endpoint.

--- siberian/app/sae/modules/Admin/controllers/Api/AccountController.php
+++ siberian/app/sae/modules/Admin/controllers/Api/AccountController.php
@@ -23,36 +23,29 @@ class Admin_Api_AccountController extends Api_Controller_Default
 
     public function existAction()
     {
+        try {
+            $request = $this->getRequest();
+            $bodyParams = $request->getPost();
 
-        if ($data = $this->getRequest()->getPost()) {
-
-            try {
-
-                if (empty($data["email"])) {
-                    throw new \Siberian\Exception(__("The email is required"));
-                }
-
-                $email = $data["email"];
-                $data = ["success" => 1];
-                $admin = new Admin_Model_Admin();
-                $admin->find($email, "email");
-
-                $data = [
-                    "success" => 1,
-                    "exists" => (bool)$admin->getId()
-                ];
-
-            } catch (Exception $e) {
-                $data = [
-                    "error" => 1,
-                    "message" => $e->getMessage()
-                ];
+            if (empty($bodyParams['email'])) {
+                throw new \Siberian\Exception(__('The email is required'));
             }
 
-            $this->_sendJson($data);
+            $admin = (new Admin_Model_Admin())->find($bodyParams['email'], 'email');
 
+            $payload = [
+                'success' => true,
+                'id' => $admin->getId(),
+                'exists' => (bool) $admin->getId()
+            ];
+        } catch (\Exception $e) {
+            $payload = [
+                'error' => true,
+                'message' => $e->getMessage()
+            ];
         }
 
+        $this->_sendJson($payload);
     }
 
     public function authenticateAction()
@@ -190,7 +183,6 @@ class Admin_Api_AccountController extends Api_Controller_Default
                             __("This email address is already used")
                         );
                     }
-
                 }
 
                 $admin->addData($data);
