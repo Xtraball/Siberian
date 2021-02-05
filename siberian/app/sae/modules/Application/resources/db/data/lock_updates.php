344371c0308dbb30a7e5cf426c536d488a0ce4c3
Alex-Yves CREPIN <a.crepin@xtraball.com>
2021-02-05 14:58:39 +0100

    feat: removed option from editor.

--- siberian/app/sae/modules/Application/resources/db/data/lock_updates.php
+++ siberian/app/sae/modules/Application/resources/db/data/lock_updates.php
@@ -1,7 +1,7 @@
 <?php
 
 // Update 4.20.3
-$query = "UPDATE `application` SET `disable_updates` = 1 WHERE 1;";
+$query = "UPDATE `application` SET `disable_updates` = 1;";
 
 $disablePatch = __get('force_lock_update');
 if ($disablePatch !== 'fixed') {
