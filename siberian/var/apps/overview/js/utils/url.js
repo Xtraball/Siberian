e2dfab06c6bc7d1bdddff78d5941c0143b60567a
Alex-Yves CREPIN <a.crepin@xtraball.com>
2021-06-25 15:41:59 +0200

    chore: rebuild browser

--- siberian/var/apps/overview/js/utils/url.js
+++ siberian/var/apps/overview/js/utils/url.js
@@ -8,6 +8,7 @@ var fbtoken = window.location.hash.match(/\?__tokenfb__=(.*)/);
 var LOGIN_FB = false;
 var IS_NATIVE_APP = false;
 var DEVICE_TYPE = 3;
+var XS_VERSION = false;
 var AVAILABLE_LANGUAGES = ['en'];
 var DISABLE_BATTERY_OPTIMIZATION = false;
 
