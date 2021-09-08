310821b95e1e1cd4e0ad11d853dc26c1da6754f6
Alex-Yves CREPIN <a.crepin@xtraball.com>
2021-09-08 16:37:00 +0200

    feat: removed unused sections of backoffice and editor.

--- siberian/app/sae/modules/Application/Model/SourceQueue.php
+++ siberian/app/sae/modules/Application/Model/SourceQueue.php
@@ -114,36 +114,32 @@ class Application_Model_SourceQueue extends Core_Model_Default
                 $this->setPath($result);
                 $this->save();
 
-                // If autopublish, send to job, but no e-mail, jenkins will do it!
-                if ($this->getIsAutopublish()) {
-                    $this->sendJobToAutoPublishServer($application, $result);
-                } else {
-                    // Success email!
-                    $protocol = "https://";
-                    $url = $protocol . $this->getHost() . "/" . str_replace(Core_Model_Directory::getBasePathTo(""), "", $result);
+                // Success email!
+                $protocol = "https://";
+                $url = $protocol . $this->getHost() . "/" . str_replace(Core_Model_Directory::getBasePathTo(""), "", $result);
 
-                    $baseEmail = $this->baseEmail(
-                        'source_queue_success',
-                        $application,
-                        __('Build succeed'),
-                        null);
+                $baseEmail = $this->baseEmail(
+                    'source_queue_success',
+                    $application,
+                    __('Build succeed'),
+                    null);
 
-                    $baseEmail->setContentFor('content_email', 'type', $type);
-                    $baseEmail->setContentFor('content_email', 'link', $url);
-                    $baseEmail->setContentFor('content_email', 'application_name', $application->getName());
+                $baseEmail->setContentFor('content_email', 'type', $type);
+                $baseEmail->setContentFor('content_email', 'link', $url);
+                $baseEmail->setContentFor('content_email', 'application_name', $application->getName());
 
-                    $content = $baseEmail->render();
+                $content = $baseEmail->render();
 
-                    $subject = sprintf('%s - %s',
-                        $application->getName(),
-                        __('Build succeed!'));
+                $subject = sprintf('%s - %s',
+                    $application->getName(),
+                    __('Build succeed!'));
 
-                    $mail = new \Siberian_Mail();
-                    $mail->setBodyHtml($content);
-                    $mail->addTo($recipients);
-                    $mail->setSubject($subject);
-                    $mail->send();
-                }
+                $mail = new \Siberian_Mail();
+                $mail->setBodyHtml($content);
+                $mail->addTo($recipients);
+                $mail->setSubject($subject);
+                $mail->send();
+
             } else {
                 $this->changeStatus("failed");
                 $this->save();
@@ -199,217 +195,6 @@ class Application_Model_SourceQueue extends Core_Model_Default
         ;
 
         return $layout;
-    }
-
-    /**
-     * @param $application
-     * @param $sourcePath
-     * @throws Zend_Uri_Exception
-     * @throws \Siberian\Exception
-     * @throws \rock\sanitize\SanitizeException
-     */
-    protected function sendJobToAutoPublishServer($application, $sourcePath)
-    {
-        $appId = $application->getId();
-
-        // iOS license key!
-        $licenseKey = __get('ios_autobuild_key');
-
-        // Application infos!
-        $app = (new Application_Model_Application())
-            ->find($appId);
-
-        // Backoffice user!
-        $user = (new Backoffice_Model_User())
-            ->find($this->getUserId());
-        $usermail = $user->getEmail();
-
-        // iOS setting info
-        $appIosAutopublish = (new Application_Model_IosAutopublish())
-            ->find($appId, 'app_id');
-
-        $languages = Siberian_Json::decode($appIosAutopublish->getLanguages());
-
-        if (!array_key_exists('error', $languages)) {
-            if (count($languages) === 0) {
-                throw new \Siberian\Exception('There is no language selected');
-            }
-        } else {
-            throw new \Siberian\Exception('Cannot unserialize language data');
-        }
-
-        //we keep using ISO-639 for siberian storage but we have to translate ISO code to faslane languages name
-        $fastlane_languages = [];
-        foreach ($languages as $codeIos => $activated) {
-            $fast_label = Application_Model_Languages::getLabelFromCodeIso($codeIos);
-            $fastlane_languages[$fast_label] = $activated;
-        }
-
-        $data = [
-            'name' => $app->getName(),
-            'bundle_id' => $app->getBundleId(),
-            'want_to_autopublish' => $appIosAutopublish->getWantToAutopublish(),
-            'credentials' => $appIosAutopublish->getCypheredCredentials(),
-            'team_id' => $appIosAutopublish->getTeamId(),
-            'team_name' => $appIosAutopublish->getTeamName(),
-            'itc_provider' => $appIosAutopublish->getItcProvider(),
-            'account_type' => $appIosAutopublish->getAccountType(),
-            'original_login' => $appIosAutopublish->getItunesOriginalLogin(),
-            'has_bg_locate' => $appIosAutopublish->getHasBgLocate(),
-            'has_audio' => $appIosAutopublish->getHasAudio(),
-            'languages' => $fastlane_languages,
-            'host' => $this->getHost(),
-            'license_key' => $licenseKey,
-            'token' => $appIosAutopublish->getToken(),
-            'email' => $usermail,
-        ];
-
-        $jobCode = time() . '-' . $appIosAutopublish->getToken();
-        $jobFolder = Core_Model_Directory::getBasePathTo(self::ARCHIVE_FOLDER . $jobCode);
-
-        if (!mkdir($jobFolder, 0777, true)) {
-            throw new \Siberian\Exception('Cannot create folder ' . $jobFolder);
-        }
-
-        if (!copy($sourcePath, $jobFolder . '/sources.zip')) {
-            throw new \Siberian\Exception('Cannot copy sources to job folder');
-        }
-
-        $configJobFilePath = $jobFolder . '/config.json';
-        $json = Json::encode($data, JSON_PRETTY_PRINT);
-
-        if (!array_key_exists("error", Json::decode($json))) {
-            File::putContents($configJobFilePath, $json);
-        } else {
-            throw new \Siberian\Exception("Cannot create json config job file");
-        }
-
-        $tgzJobFilePath = $jobFolder . '.tgz';
-
-        exec("tar zcf $tgzJobFilePath -C $jobFolder sources.zip config.json", $output, $return_val);
-
-        if ($return_val !== 0) {
-            throw new \Siberian\Exception('Cannot create zip job file');
-        }
-
-        $jobUrlEncoded = base64_encode('http://' . $this->getHost() . '/var/tmp/jobs/' . $jobCode . '.tgz');
-
-        Siberian_Request::get(
-            "https://jenkins-prod02.xtraball.com/job/ios-autopublish/buildWithParameters",
-            [
-                'token' => 'O0cRwnWPjcfMmXc89SQ3RbVRPGXLQF6a',
-                'JOB_NAME' => slugify($app->getName()),
-                'SIBERIAN_JOB_URL' => $jobUrlEncoded,
-                'VERSION' => "3",
-            ]);
-
-        if (!in_array(Siberian_Request::$statusCode, [100, 200, 201])) {
-            throw new \Siberian\Exception(__('Cannot send build to service %s.', Siberian_Request::$statusCode));
-        }
-    }
-
-    /**
-     * @param $application
-     * @throws Zend_Uri_Exception
-     * @throws \Siberian\Exception
-     * @throws \rock\sanitize\SanitizeException
-     */
-    protected function sendPemToAutoPublishServer($application)
-    {
-        $appId = $application->getId();
-
-        // iOS license key!
-        $licenseKey = __get('ios_autobuild_key');
-
-        // Application infos!
-        $app = (new Application_Model_Application())
-            ->find($appId);
-
-        // Backoffice user!
-        $user = (new Backoffice_Model_User())
-            ->find($this->getUserId());
-        $usermail = $user->getEmail();
-
-        // iOS setting info
-        $appIosAutopublish = (new Application_Model_IosAutopublish())
-            ->find($appId, 'app_id');
-
-        $languages = Siberian_Json::decode($appIosAutopublish->getLanguages());
-
-        if (!array_key_exists('error', $languages)) {
-            if (count($languages) === 0) {
-                throw new \Siberian\Exception('There is no language selected');
-            }
-        } else {
-            throw new \Siberian\Exception('Cannot unserialize language data');
-        }
-
-        //we keep using ISO-639 for siberian storage but we have to translate ISO code to faslane languages name
-        $fastlane_languages = [];
-        foreach ($languages as $codeIos => $activated) {
-            $fast_label = Application_Model_Languages::getLabelFromCodeIso($codeIos);
-            $fastlane_languages[$fast_label] = $activated;
-        }
-
-        $data = [
-            'name' => $app->getName(),
-            'bundle_id' => $app->getBundleId(),
-            'want_to_autopublish' => $appIosAutopublish->getWantToAutopublish(),
-            'credentials' => $appIosAutopublish->getCypheredCredentials(),
-            'team_id' => $appIosAutopublish->getTeamId(),
-            'team_name' => $appIosAutopublish->getTeamName(),
-            'itc_provider' => $appIosAutopublish->getItcProvider(),
-            'has_bg_locate' => $appIosAutopublish->getHasBgLocate(),
-            'has_audio' => $appIosAutopublish->getHasAudio(),
-            'languages' => $fastlane_languages,
-            'host' => $this->getHost(),
-            'license_key' => $licenseKey,
-            'token' => $appIosAutopublish->getToken(),
-            'email' => $usermail,
-            'refresh_pem' => true,
-        ];
-
-        $jobCode = time() . '-' . $appIosAutopublish->getToken();
-        $jobFolder = Core_Model_Directory::getBasePathTo(self::ARCHIVE_FOLDER . $jobCode);
-
-        if (!mkdir($jobFolder, 0777, true)) {
-            throw new \Siberian\Exception('Cannot create folder ' . $jobFolder);
-        }
-
-        $fakeSources = Core_Model_Directory::getBasePathTo('/var/apps/ionic/refresh_pem.zip');
-        if (!copy($fakeSources, $jobFolder . '/sources.zip')) {
-            throw new \Siberian\Exception('Cannot copy sources to job folder');
-        }
-
-        $configJobFilePath = $jobFolder . '/config.json';
-        $json = Siberian_Json::encode($data, JSON_PRETTY_PRINT);
-
-        if (!array_key_exists('error', $json)) {
-            File::putContents($configJobFilePath, $json);
-        } else {
-            throw new \Siberian\Exception('Cannot create json config job file');
-        }
-
-        $tgzJobFilePath = $jobFolder . '.tgz';
-
-        exec("tar zcf $tgzJobFilePath -C $jobFolder sources.zip config.json", $output, $return_val);
-
-        if ($return_val !== 0) {
-            throw new \Siberian\Exception('Cannot create zip job file');
-        }
-
-        $jobUrlEncoded = base64_encode('http://' . $this->getHost() . '/var/tmp/jobs/' . $jobCode . '.tgz');
-
-        Siberian_Request::get(
-            "https://jenkins-prod02.xtraball.com/job/generate-pem/buildWithParameters",
-            [
-                'token' => '6EJQwGkCLzTTvSWUfY19a3QshNvk8RXK',
-                'SIBERIAN_JOB_URL' => $jobUrlEncoded,
-            ]);
-
-        if (!in_array(Siberian_Request::$statusCode, [100, 200, 201])) {
-            throw new \Siberian\Exception(__('Cannot send build to service %s.', Siberian_Request::$statusCode));
-        }
     }
 
     /**
