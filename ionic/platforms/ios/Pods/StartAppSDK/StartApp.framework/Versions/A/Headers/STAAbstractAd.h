bb15db292999492cd9644854060a159e62d5812f
Alex-Yves CREPIN <a.crepin@xtraball.com>
2021-11-03 11:39:11 +0100

    chore: rebuilt ios

---
+++ ionic/platforms/ios/Pods/StartAppSDK/StartApp.framework/Versions/A/Headers/STAAbstractAd.h
@@ -0,0 +1,34 @@
+//
+//  STAAbstractAd.h
+//  StartAppAdSDK
+//
+//  Copyright (c) 2013 StartApp. All rights reserved.
+//  SDK version 4.6.6
+
+#import <Foundation/Foundation.h>
+
+@class STAAbstractAd;
+@class STANativeAdDetails;
+
+@protocol STADelegateProtocol <NSObject>
+
+@optional
+- (void)didLoadAd:(STAAbstractAd *)ad;
+- (void)failedLoadAd:(STAAbstractAd *)ad withError:(NSError *)error;
+- (void)didShowAd:(STAAbstractAd *)ad;
+- (void)failedShowAd:(STAAbstractAd *)ad withError:(NSError *)error;
+- (void)didCloseAd:(STAAbstractAd *)ad;
+- (void)didClickAd:(STAAbstractAd *)ad;
+- (void)didCloseInAppStore:(STAAbstractAd *)ad;
+- (void)didCompleteVideo:(STAAbstractAd *)ad;
+
+- (void)didShowNativeAdDetails:(STANativeAdDetails *)nativeAdDetails;
+- (void)didClickNativeAdDetails:(STANativeAdDetails *)nativeAdDetails;
+
+@end
+
+@interface STAAbstractAd : NSObject
+
+- (BOOL) isReady;
+
+@end
