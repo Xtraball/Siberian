65cbde98c1b34830d2e03814aee70a0bce29df93
Alex-Yves CREPIN <a.crepin@xtraball.com>
2022-03-09 10:24:00 +0100

    fix: php 7.1 req, lyaout 18 not scrolling

--- siberian/lib/vendor/composer/platform_check.php
+++ siberian/lib/vendor/composer/platform_check.php
@@ -5,7 +5,7 @@
 $issues = array();
 
 if (!(PHP_VERSION_ID >= 70103)) {
-    $issues[] = 'Your Composer dependencies require a PHP version ">= 7.1.3". You are running ' . PHP_VERSION . '.';
+    //$issues[] = 'Your Composer dependencies require a PHP version ">= 7.1.3". You are running ' . PHP_VERSION . '.';
 }
 
 if ($issues) {
