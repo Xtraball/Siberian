525d92559657b9aa3a3eb2fa18fcfb05c48284b7
Alex-Yves CREPIN <a.crepin@xtraball.com>
2022-08-02 16:13:40 +0200

    chore: rebuild ios

--- ionic/platforms/ios/platform_www/cordova_plugins.js
+++ ionic/platforms/ios/platform_www/cordova_plugins.js
@@ -7,6 +7,14 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
       "runs": true
     },
     {
+      "id": "Admob.AdMob",
+      "file": "plugins/Admob/www/admob.js",
+      "pluginId": "Admob",
+      "clobbers": [
+        "admob"
+      ]
+    },
+    {
       "id": "AppVersion.AppVersionPlugin",
       "file": "plugins/AppVersion/www/AppVersionPlugin.js",
       "pluginId": "AppVersion",
@@ -447,6 +455,7 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
   ];
   module.exports.metadata = {
     "Promises": "4.2.2",
+    "Admob": "0.0.0",
     "AppVersion": "0.1.8",
     "Badge": "0.8.8",
     "BarcodeScanner": "7.1.2",
@@ -471,6 +480,7 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
     "InAppPurchase": "10.6.1",
     "IosXhr": "1.0.8",
     "Push": "1.10.0",
+    "Mediation": "1.0.0",
     "WebView": "1.0.0"
   };
 });
