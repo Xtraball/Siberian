1b9bb7ac90d6233da61d40b4eda2c2b748b319cb
Siberian Admin <dev@xtraball.com>
2023-02-11 18:35:19 +0100

    feat: rebuilt ios noads

---
+++ ionic/platforms/ios/www/js/controllers/push.js
@@ -0,0 +1,11 @@
+// DUMMY DEPRECATED
+angular.module('starter').controller('PushController', function ($scope) {
+    angular.extend($scope, {});
+    $scope.loadContent = function (loadMore) {};
+    $scope.pullToRefresh = function () {};
+    $scope.deletePush = function (item) {};
+    $scope.showItem = function (item) {};
+    $scope.hasItem = function (item) { return false };
+    $scope.loadMore = function () {};
+    $scope.loadContent();
+});
