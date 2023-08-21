780110829012519f8d9f6ec088cfd37f430c1b97
Xtraball SAS <dev@xtraball.com>
2023-08-21 16:34:32 +0200

    hotfix: patching critical RCE in all versions.

--- siberian/app/sae/modules/Firewall/Model/Rule.php
+++ siberian/app/sae/modules/Firewall/Model/Rule.php
@@ -24,4 +24,93 @@ class Firewall_Model_Rule extends Core_Model_Default
         $this->_db_table = 'Firewall_Model_Db_Table_Rule';
         return $this;
     }
+
+    /**
+     * @param string|null $extension
+     * @return void
+     * @throws \Siberian\Exception
+     */
+    public static function allowExtension(string $extension = null)
+    {
+        $extension = self::checkRules($extension);
+
+        $fwRule = (new self())->find([
+            'type' => self::FW_TYPE_UPLOAD,
+            'value' => $extension
+        ]);
+
+        if (!$fwRule->getId()) {
+            $fwRule
+                ->setType(self::FW_TYPE_UPLOAD)
+                ->setValue($extension)
+                ->save();
+        }
+    }
+
+    /**
+     * @param string|null $extension
+     * @return void
+     * @throws Zend_Exception
+     * @throws \Siberian\Exception
+     */
+    public static function disallowExtension(string $extension = null)
+    {
+        $extension = self::checkRules($extension);
+
+        $fwRule = (new self())->find([
+            'type' => self::FW_TYPE_UPLOAD,
+            'value' => $extension
+        ]);
+
+        if ($fwRule->getId()) {
+            $fwRule->delete();
+        }
+    }
+
+    /**
+     * @return array
+     */
+    public static function listExtensions()
+    {
+        $fwRules = (new self())->findAll([
+            'type' => self::FW_TYPE_UPLOAD,
+        ]);
+
+        $extensions = [];
+        foreach ($fwRules as $fwRule) {
+            $extensions[] = $fwRule->getValue();
+        }
+
+        return $extensions;
+    }
+
+    /**
+     * @param string|null $extension
+     * @return string
+     * @throws Zend_Exception
+     * @throws \Siberian\Exception
+     */
+    public static function checkRules(string $extension = null)
+    {
+        if (__getConfig('is_demo')) {
+            // Demo version
+            throw new Exception(__("You cannot change Firewall settings, it's a demo version."));
+        }
+
+        if (empty($extension)) {
+            throw new \Siberian\Exception(__('Missing value'));
+        }
+
+        $extension = trim($extension);
+        if (empty($extension)) {
+            throw new \Siberian\Exception(__("Extension can't be empty."));
+        }
+
+        if (in_array($extension, ['php', 'js', 'ico'])) {
+            throw new \Siberian\Exception(__("Extension %s is strictly forbidden.", $extension));
+        }
+
+        return $extension;
+    }
+
 }
--- siberian/app/sae/modules/Firewall/controllers/IndexController.php
+++ siberian/app/sae/modules/Firewall/controllers/IndexController.php
@@ -139,58 +139,6 @@ class Firewall_IndexController extends Backoffice_Controller_Default
         $this->_sendJson($payload);
     }
 
-    public function addfwuploadruleAction()
-    {
-        try {
-            if (__getConfig('is_demo')) {
-                // Demo version
-                throw new Exception(__("You cannot change Firewall settings, it's a demo version."));
-            }
-
-            $request = $this->getRequest();
-            $params = $request->getBodyParams();
-
-            if (empty($params)) {
-                throw new \Siberian\Exception(__('Missing value'));
-            }
-
-            $value = trim($params['value']);
-
-            if (empty($value)) {
-                throw new \Siberian\Exception(__("Extension can't be empty."));
-            }
-
-            if (in_array($value, ['php', 'js', 'ico'])) {
-                throw new \Siberian\Exception(__("Extension %s is strictly forbidden.", $value));
-            }
-
-            $fwRule = (new \Firewall_Model_Rule())->find([
-                'type' => \Firewall_Model_Rule::FW_TYPE_UPLOAD,
-                'value' => $value
-            ]);
-
-            if (!$fwRule->getId()) {
-                $fwRule
-                    ->setType(\Firewall_Model_Rule::FW_TYPE_UPLOAD)
-                    ->setValue($value)
-                    ->save();
-            }
-
-            $payload = [
-                'success' => true,
-                'message' => __('Rule have been added.'),
-            ];
-
-        } catch (\Exception $e) {
-            $payload = [
-                'error' => true,
-                'message' => $e->getMessage(),
-            ];
-        }
-
-        $this->_sendJson($payload);
-    }
-
     public function savefwclamdsettingsAction()
     {
         try {
