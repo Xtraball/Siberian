c5f8828b17841fabc630fe70fbfcef98b2923834
Anders <alex@kipsoft.fr>
2023-08-24 11:13:26 +0000

    fix: possible SQLi in order clause.

--- siberian/app/sae/modules/Admin/controllers/Backoffice/ListController.php
+++ siberian/app/sae/modules/Admin/controllers/Backoffice/ListController.php
@@ -27,34 +27,34 @@ class Admin_Backoffice_ListController extends Backoffice_Controller_Default
         try {
             $request = $this->getRequest();
             $offset = $request->getParam('offset', null);
-        $limit = Admin_Model_Admin::BO_DISPLAYED_PER_PAGE;
+            $limit = Admin_Model_Admin::BO_DISPLAYED_PER_PAGE;
             $range = $request->getHeader('Range');
 
             // Default range!
             $parts = [0, 20];
             if ($range) {
                 $parts = explode('-', $range);
-            $offset = $parts[0];
-            $limit = ($parts[1] - $parts[0]) + 1;
-        }
+                $offset = $parts[0];
+                $limit = ($parts[1] - $parts[0]) + 1;
+            }
 
-        $params = [
+            $params = [
                 'offset' => $offset,
                 'limit' => $limit
-        ];
+            ];
 
-        $filters = [];
+            $filters = [];
             $_filter = $request->getParam('filter', false);
             if ($_filter) {
                 $filters["(a.email LIKE ? OR a.admin_id LIKE ? OR a.company LIKE ? OR a.website LIKE ? OR a.firstname LIKE ? OR a.lastname LIKE ? OR r.code LIKE ? OR r.label LIKE ?)"] = "%{$_filter}%";
-        }
+            }
 
             $order = $request->getParam('order', false);
             $by = filter_var($request->getParam('by', false), FILTER_VALIDATE_BOOLEAN);
-        if ($order) {
+            if ($order) {
                 $order_by = ($by) ? 'ASC' : 'DESC';
-            $order = sprintf("%s %s", $order, $order_by);
-        }
+                $order = sprintf("%s %s", $order, $order_by);
+            }
 
             $allAdmins = (new Admin_Model_Admin())->findAllForBackoffice($filters, $order);
             $total = $allAdmins->count();
@@ -62,15 +62,15 @@ class Admin_Backoffice_ListController extends Backoffice_Controller_Default
             $admins = (new Admin_Model_Admin())->findAllForBackoffice($filters, $order, $params);
 
             if ($range) {
-            $start = $parts[0];
-            $end = ($total <= $parts[1]) ? $total : $parts[1];
+                $start = $parts[0];
+                $end = ($total <= $parts[1]) ? $total : $parts[1];
 
                 $this->getResponse()->setHeader('Content-Range', sprintf("%s-%s/%s", $start, $end, $total));
                 $this->getResponse()->setHeader('Range-Unit', 'items');
-        }
+            }
 
             $payload = [];
-        foreach ($admins as $admin) {
+            foreach ($admins as $admin) {
                 $payload[] = [
                     'id' => $admin->getId(),
                     'email' => $admin->getEmail(),
