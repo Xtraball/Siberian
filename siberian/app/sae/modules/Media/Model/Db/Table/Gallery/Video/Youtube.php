28f4c7ddc1280831d032c158274e9e7d68131206
Pesterev Andrey <pesterev.andruha@gmail.com>
2023-11-08 11:08:03 +0300

    Error when update to php 8.0 and more

--- siberian/app/sae/modules/Media/Model/Db/Table/Gallery/Video/Youtube.php
+++ siberian/app/sae/modules/Media/Model/Db/Table/Gallery/Video/Youtube.php
@@ -5,7 +5,7 @@ class Media_Model_Db_Table_Gallery_Video_Youtube extends Core_Model_Db_Table {
     protected $_name = "media_gallery_video_youtube";
     protected $_primary = "gallery_id";
 
-    public function getFields() {
+    public function getFields($tablename = null) {
         $fields = array_keys($this->_db->describeTable($this->_name));
         return array_combine($fields, $fields);
     }
